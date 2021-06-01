c82ed6b7e148785bf40f6ec3bf717de64ec45c16
Francisco Giordano <frangio.1@gmail.com>
2021-06-01 19:39:01 -0300

    Use code splitting to speed up first paint (#32)

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -6,8 +6,6 @@ export { ContractBuilder } from './contract';
 export { printContract } from './print';
 export { printContractVersioned } from './print-versioned';
 
-export { zipContract } from './zip';
-
 export type { Access } from './set-access-control';
 
 export { premintPattern } from './erc20';
