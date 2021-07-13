3a4b26ce82bb10cf25712cfa81bf684acaee3892
Francisco Giordano <frangio.1@gmail.com>
2021-07-13 17:54:57 -0300

    fix versioning for remix

--- packages/core/src/print-versioned.ts
+++ packages/core/src/print-versioned.ts
@@ -5,7 +5,7 @@ import { printContract } from "./print";
 export function printContractVersioned(contract: Contract): string {
   return printContract(contract, {
     transformImport: p =>
-      p.replace(/^@openzeppelin\/contracts/, `$&@${contractsVersion}`),
+      p.replace(/^@openzeppelin\/contracts(-upgradeable)?/, `$&@${contractsVersion}`),
   });
 }
 
