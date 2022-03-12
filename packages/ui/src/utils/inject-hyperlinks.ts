25b7daeaab97c0ec44ebbc4c55eccf4e51ff31fc
Francisco Giordano <frangio.1@gmail.com>
2022-03-12 00:24:52 -0300

    move css to class

--- packages/ui/src/utils/inject-hyperlinks.ts
+++ packages/ui/src/utils/inject-hyperlinks.ts
@@ -2,5 +2,5 @@ import { version as contractsVersion } from "@openzeppelin/contracts/package.jso
 
 export function injectHyperlinks(code: string) {
   const importRegex = /(@openzeppelin\/)(contracts-upgradeable\/|contracts\/)(.*)(&quot;)/g // we are modifying HTML, so use HTML escaped chars
-  return code.replace(importRegex, `<a href='https://github.com/OpenZeppelin/openzeppelin-$2blob/v${contractsVersion}/contracts/$3' target='_blank' rel='noopener noreferrer'>$1$2$3</a>$4`);
+  return code.replace(importRegex, `<a class="import-link" href="https://github.com/OpenZeppelin/openzeppelin-$2blob/v${contractsVersion}/contracts/$3" target="_blank" rel="noopener noreferrer">$1$2$3</a>$4`);
 }
