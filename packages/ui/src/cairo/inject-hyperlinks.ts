b16fbec787957f4172a77d2c55a998097b6d1916
Eric Lau <ericglau@outlook.com>
2022-03-31 10:34:37 -0400

    Add starkware import links

--- packages/ui/src/cairo/inject-hyperlinks.ts
+++ packages/ui/src/cairo/inject-hyperlinks.ts
@@ -1,12 +1,15 @@
 export function injectHyperlinks(code: string) {
-  const importRegex = /( )(openzeppelin)([^\s]*)( )/g // we are modifying HTML, so use HTML escaped chars
+  const importRegex = /( )(openzeppelin|starkware)([^\s]*)( )/g
   let result = code;
   let match = importRegex.exec(code);
   while (match != null) {
-    const [line, spaceBefore, ozPrefix, contractsLibrary, spaceAfter] = match;
-    if (line !== undefined && spaceBefore !== undefined && ozPrefix !== undefined && contractsLibrary !== undefined && spaceAfter !== undefined) {
-      const libraryRelativePath = contractsLibrary.replace(/\./g, '/');
-      const replacedImportLine = `${spaceBefore}<a class="import-link" href='https://github.com/OpenZeppelin/cairo-contracts/blob/main/src/${ozPrefix}${libraryRelativePath}.cairo' target='_blank' rel='noopener noreferrer'>${ozPrefix}${contractsLibrary}</a>${spaceAfter}`;
+    const [line, spaceBefore, libraryPrefix, libraryPath, spaceAfter] = match;
+    if (line !== undefined && spaceBefore !== undefined && libraryPrefix !== undefined && libraryPath !== undefined && spaceAfter !== undefined) {
+      const libraryRelativePath = libraryPath.replace(/\./g, '/');
+      const githubPrefix = (libraryPrefix === 'openzeppelin') ? 
+        'https://github.com/OpenZeppelin/cairo-contracts/blob/main/src/' :
+        'https://github.com/starkware-libs/cairo-lang/blob/master/src/';
+      const replacedImportLine = `${spaceBefore}<a class="import-link" href='${githubPrefix}${libraryPrefix}${libraryRelativePath}.cairo' target='_blank' rel='noopener noreferrer'>${libraryPrefix}${libraryPath}</a>${spaceAfter}`;
       result = result.replace(line, replacedImportLine);
     }
     match = importRegex.exec(code);
