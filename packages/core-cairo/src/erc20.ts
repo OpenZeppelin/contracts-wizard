45601c2a6f39bd276d0bd0e7e281054b0e7edfcd
Eric Lau <ericglau@outlook.com>
2022-04-06 15:51:28 -0400

    Cleanup, remove solidity tests from cairo

--- packages/core-cairo/src/erc20.ts
+++ packages/core-cairo/src/erc20.ts
@@ -12,13 +12,9 @@ export interface ERC20Options extends CommonOptions {
   name: string;
   symbol: string;
   burnable?: boolean;
-  snapshots?: boolean;
   pausable?: boolean;
   premint?: string;
   mintable?: boolean;
-  permit?: boolean;
-  votes?: boolean;
-  flashmint?: boolean;
   decimals?: string;
 }
 
@@ -70,19 +66,9 @@ export function buildERC20(opts: ERC20Options): Contract {
     false
   );
 
-  // c.addFunctionCode(`# Cairo equivalent to 'return (true)'`, functions.transfer);
-  // c.addFunctionCode(`# Cairo equivalent to 'return (true)'`, functions.transferFrom);
-  // c.addFunctionCode(`# Cairo equivalent to 'return (true)'`, functions.approve);
-  // c.addFunctionCode(`# Cairo equivalent to 'return (true)'`, functions.increaseAllowance);
-  // c.addFunctionCode(`# Cairo equivalent to 'return (true)'`, functions.decreaseAllowance);
-
   if (opts.burnable) {
     addBurnable(c);
   }
-
-  // if (opts.snapshots) {
-  //   addSnapshot(c, access);
-  // }
 
   if (opts.pausable) {
     addPausable(c, access, [functions.transfer, functions.transferFrom, functions.approve, functions.increaseAllowance, functions.decreaseAllowance]);
@@ -99,20 +85,7 @@ export function buildERC20(opts: ERC20Options): Contract {
     addMintable(c, access);
   }
 
-  // // Note: Votes requires Permit
-  // if (opts.permit || opts.votes) {
-  //   addPermit(c, opts.name);
-  // }
-
-  // if (opts.votes) {
-  //   addVotes(c);
-  // }
-
-  // if (opts.flashmint) {
-  //   addFlashMint(c);
-  // }
-
-  setUpgradeable(c, upgradeable); //, access);
+  setUpgradeable(c, upgradeable);
   
   setInfo(c, info);
 
@@ -147,18 +120,6 @@ function addBurnable(c: ContractBuilder) {
      functions.burn
   );
 }
-
-// function addSnapshot(c: ContractBuilder, access: Access) {
-//   c.addParent({
-//     name: 'ERC20Snapshot',
-//     path: 'contracts/token/ERC20/extensions/ERC20Snapshot',
-//   });
-
-//   c.addOverride('ERC20Snapshot', functions._beforeTokenTransfer);
-
-//   setAccessControl(c, functions.snapshot, access, 'SNAPSHOT');
-//   c.addFunctionCode('_snapshot();', functions.snapshot);
-// }
 
 export const premintPattern = /^(\d*)(?:\.(\d+))?(?:e(\d+))?$/; // TODO don't allow exponent?
 
@@ -237,33 +198,7 @@ function getPremintAbsolute(premint: string, decimals: number): string {
 
 function addMintable(c: ContractBuilder, access: Access) {
   setAccessControl(c, functions.mint, access, 'MINTER');
-  //c.addFunctionCode('_mint(to, amount);', functions.mint);
 }
-
-// function addPermit(c: ContractBuilder, name: string) {
-//   c.addParent({
-//     name: 'ERC20Permit',
-//     path: 'contracts/token/ERC20/extensions/draft-ERC20Permit',
-//   }, [name]);
-// }
-
-// function addVotes(c: ContractBuilder) {
-//   if (!c.parents.some(p => p.contract.name === 'ERC20Permit')) {
-//     throw new Error('Missing ERC20Permit requirement for ERC20Votes');
-//   }
-
-//   c.addParent({
-//     name: 'ERC20Votes',
-//     path: 'contracts/token/ERC20/extensions/ERC20Votes',
-//   });
-// }
-
-// function addFlashMint(c: ContractBuilder) {
-//   c.addParent({
-//     name: 'ERC20FlashMint',
-//     path: 'contracts/token/ERC20/extensions/ERC20FlashMint',
-//   });
-// }
 
 const functions = defineFunctions({
 
@@ -410,24 +345,8 @@ const functions = defineFunctions({
     kind: 'external' as const,
     implicitArgs: withImplicitArgs(),
     args: [
-      // { name: 'account', type: 'felt' },
       { name: 'amount', type: 'Uint256' },
     ],
   },
 
-
-  // pause: {
-  //   kind: 'external' as const,
-  //   args: [],
-  // },
-
-  // unpause: {
-  //   kind: 'external' as const,
-  //   args: [],
-  // },
-
-  // snapshot: {
-  //   kind: 'external' as const,
-  //   args: [],
-  // },
 });
