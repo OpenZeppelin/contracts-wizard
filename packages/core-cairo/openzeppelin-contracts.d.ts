7fabcf83a069010f5fd7f436d4f8df5f38fc7359
Eric Lau <ericglau@outlook.com>
2022-04-12 14:59:10 -0400

    Remove unused solidity content from cairo

--- packages/core-cairo/openzeppelin-contracts.d.ts
+++
@@ -1,9 +0,0 @@
-export interface OpenZeppelinContracts {
-  version: string;
-  sources: Record<string, string>;
-  dependencies: Record<string, string[]>;
-}
-
-declare const contracts: OpenZeppelinContracts;
-
-export default contracts;
