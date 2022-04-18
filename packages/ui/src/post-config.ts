ee4aec24d5349550e67f653c080a626e81c817ee
Francisco Giordano <frangio.1@gmail.com>
2022-04-18 11:19:40 -0300

    remove console.log comments

--- packages/ui/src/post-config.ts
+++ packages/ui/src/post-config.ts
@@ -13,7 +13,6 @@ export type Action = 'copy' | 'remix' | 'download-npm' | 'download-vendored';
 // NOTE: We have to make sure any fields sent in the body are defined in the
 // hidden form in public/index.html.
 export async function postConfig(opts: Required<GenericOptions>, action: Action) {
-  //console.log("running postConfig... ");
   sendAnalyticsToGTM(opts, action);
   await fetch('/config', {
     method: 'POST',
@@ -29,16 +28,13 @@ export async function postConfig(opts: Required<GenericOptions>, action: Action)
   });
 }
 function sendAnalyticsToGTM(opts: any, action : Action){
-  //console.log("sendAnalyticsToGTM..." + JSON.stringify(opts));
   var currentObject = opts;
   
   if (typeof window.gtag === 'undefined') {
-    //console.log("no GTAG found --- undefined");
     return;
   } 
    
   opts.action = action;
-  //console.log("opts summary : " + JSON.stringify(opts));
   window.gtag("event", "wizard_action", opts);
 }
 function encode<K extends string, V extends string | number | boolean>(
