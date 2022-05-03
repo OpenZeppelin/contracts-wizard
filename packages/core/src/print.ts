717e116ed5c97cf71f9607d63b4db21ecaef26ff
Eric Lau <ericglau@outlook.com>
2022-05-03 19:41:49 -0400

    Add initializer call for ERC20Votes (#124)

--- packages/core/src/print.ts
+++ packages/core/src/print.ts
@@ -103,7 +103,7 @@ function hasInitializer(parent: Parent) {
   // CAUTION
   // This list is validated by compilation of SafetyCheck.sol.
   // Always keep this list and that file in sync.
-  return !['Initializable', 'ERC20Votes'].includes(parent.contract.name);
+  return !['Initializable'].includes(parent.contract.name);
 }
 
 type SortedFunctions = Record<'code' | 'modifiers' | 'override', ContractFunction[]>;
