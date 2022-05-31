fbb55c6f8415768b2997d44ba1539dcec5bf0667
Francisco Giordano <frangio.1@gmail.com>
2022-05-31 13:25:36 -0300

    remove netlify-based stats

--- packages/ui/src/post-config.ts
+++ packages/ui/src/post-config.ts
@@ -12,23 +12,8 @@ const instance = uuid();
 export type Action = 'copy' | 'remix' | 'download-npm' | 'download-vendored';
 export type Language = 'solidity' | 'cairo';
 
-// NOTE: We have to make sure any fields sent in the body are defined in the
-// hidden form in public/index.html.
 export async function postConfig(opts: Required<GenericOptions> | Required<CairoOptions>, action: Action, language: Language) {
   sendAnalyticsToGTM(opts, action, language);
-  await fetch('/config', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: encode({
-      'form-name': 'config',
-      instance,
-      action,
-      data: JSON.stringify(opts),
-      language
-    }),
-  });
 }
 function sendAnalyticsToGTM(opts: any, action: Action, language: Language) {
   var currentObject = opts;
@@ -40,15 +25,4 @@ function sendAnalyticsToGTM(opts: any, action: Action, language: Language) {
   opts.action = action;
   opts.language = language;
   window.gtag("event", "wizard_action", opts);
-}
-function encode<K extends string, V extends string | number | boolean>(
-  data: Record<K, V>,
-): string {
-  const components = [];
-  for (const key in data) {
-    components.push(
-      encodeURIComponent(key) + '=' + encodeURIComponent(data[key]),
-    );
-  }
-  return components.join('&');
 }
