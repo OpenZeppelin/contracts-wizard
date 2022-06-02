9a1f2c9696376c61b5d94dc502abc5845a59eabe
Eric Lau <ericglau@outlook.com>
2022-06-02 16:18:13 -0400

    Use namespace for Proxy

--- packages/core-cairo/src/set-upgradeable.ts
+++ packages/core-cairo/src/set-upgradeable.ts
@@ -3,6 +3,7 @@ import type { ContractBuilder } from './contract';
 //import { Access, setAccessControl } from './set-access-control';
 import { defineFunctions } from './utils/define-functions';
 import { defineModules } from './utils/define-modules';
+import { defineNamespaces } from './utils/define-namespaces';
 
 export const upgradeableOptions = [false, true] as const;
 
@@ -15,15 +16,13 @@ export function setUpgradeable(c: ContractBuilder, upgradeable: Upgradeable) {
 
   c.upgradeable = true;
 
-  c.addModule(modules.Proxy, [ {lit: 'proxy_admin' }]);
-  c.addModuleFunction(modules.Proxy, 'Proxy_only_admin');
-  c.addModuleFunction(modules.Proxy, 'Proxy_set_implementation');
+  c.addModule(modules.Proxy, [ {lit: 'proxy_admin' }], [], true, namespaces.Proxy);
 
   c.addConstructorArgument({ name:'proxy_admin', type:'felt' });
 
   c.setFunctionBody([
-    'Proxy_only_admin()',
-    'Proxy_set_implementation(new_implementation)'
+    'Proxy.assert_only_admin()',
+    'Proxy._set_implementation(new_implementation)'
   ], functions.upgrade);
 }
 
@@ -34,9 +33,16 @@ const modules = defineModules( {
   },
 });
 
+const namespaces = defineNamespaces( {
+  Proxy: {
+    module: modules.Proxy,
+  },
+})
+
 const functions = defineFunctions({
 
   upgrade: {
+    namespace: namespaces.Proxy,
     kind: 'external' as const,
     implicitArgs: withImplicitArgs(),
     args: [
