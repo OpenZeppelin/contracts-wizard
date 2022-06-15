758905089e2564fc243fa8f65e17bdd348867b2e
Eric Lau <ericglau@outlook.com>
2022-06-15 10:28:53 -0400

    Remove unneeded if condition

--- packages/core-cairo/src/utils/uint256.ts
+++ packages/core-cairo/src/utils/uint256.ts
@@ -10,15 +10,11 @@ export function toUint256(num: string) {
   const bignum = new BN(num, 10);
   if (bignum.bitLength() > 256) { // 256 bits
     throw new NumberTooLarge();
-  } else if (bignum !== new BN(0)) {
+  } else {
     const highBits = bignum.shrn(128);
     const lowBits = bignum.maskn(128);
     return {
       lowBits, highBits
-    }
-  } else {
-    return {
-      lowBits: new BN(0), highBits: new BN(0)
     }
   }
 }
