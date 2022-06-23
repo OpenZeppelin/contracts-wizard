92c8712ec30db4213ea65f23445c56ba1fb90a8b
Eric Lau <ericglau@outlook.com>
2022-06-23 11:45:27 -0400

    Add UUPS radio button

--- packages/ui/src/cairo/UpgradeabilitySection.svelte
+++ packages/ui/src/cairo/UpgradeabilitySection.svelte
@@ -4,6 +4,16 @@
   import HelpTooltip from '../HelpTooltip.svelte';
 
   export let upgradeable: Upgradeable;
+
+  // Syncs UUPS radio button with Upgradeability checkbox
+  let proxyKind: false | 'uups';
+  $: {
+    proxyKind = upgradeable ? 'uups' : false;
+  }
+
+  function enableUpgradability() {
+    upgradeable = true;
+  }
 </script>
 
 <section class="controls-section">
@@ -14,10 +24,20 @@
       <span class="ml-1">
         <input type="checkbox" bind:checked={upgradeable}>
       </span>
-      <HelpTooltip align="right" link="https://github.com/OpenZeppelin/cairo-contracts/blob/main/docs/Proxies.md#using-proxies">
+      <HelpTooltip align="right" link="https://github.com/OpenZeppelin/cairo-contracts/blob/main/docs/Proxies.md">
       Smart contracts are immutable by default unless deployed behind an upgradeable proxy.
       </HelpTooltip>
     </label>
   </h1>
+
+  <div class="checkbox-group">
+    <label class:checked={upgradeable === true}>
+      <input type="radio" bind:group={proxyKind} value='uups' on:change={enableUpgradability}>
+      UUPS
+      <HelpTooltip link="https://github.com/OpenZeppelin/cairo-contracts/blob/main/docs/Proxies.md#proxy-contract">
+      Requires including code in your contract for upgrades. Allows flexibility for authorizing upgrades.
+      </HelpTooltip>
+    </label>
+  </div>
 </section>
 
