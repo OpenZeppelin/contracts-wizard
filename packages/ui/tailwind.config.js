d9e969bb7cd1c16f9a364ae800a53bb396f438ab
Francisco Giordano <frangio.1@gmail.com>
2022-08-23 18:43:34 -0300

    Fix infinite loop in build

--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  content: [ './src/**/*.{html,svelte}', './public/**/*.html' ],
+  content: [
+    './src/**/*.{html,svelte}',
+
+    // Using glob patterns results in infinite loop
+    './public/index.html',
+    './public/cairo.html',
+    './public/embed.html',
+  ],
 };
