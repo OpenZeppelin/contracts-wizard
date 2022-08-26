895d60a1e8ce9a95441bb22ded6428fc86882818
Francisco Giordano <frangio.1@gmail.com>
2022-08-26 14:20:46 -0300

    undo linting

--- packages/ui/src/cairo/App.svelte
+++ packages/ui/src/cairo/App.svelte
@@ -1,282 +1,282 @@
 <script lang="ts">
-  import { createEventDispatcher } from 'svelte';
+    import { createEventDispatcher } from 'svelte';
 
-  import hljs from './highlightjs';
+    import hljs from './highlightjs';
 
-  import ERC20Controls from './ERC20Controls.svelte';
-  import ERC721Controls from './ERC721Controls.svelte';
-  import CustomControls from './CustomControls.svelte';
-  import CopyIcon from '../icons/CopyIcon.svelte';
-  import CheckIcon from '../icons/CheckIcon.svelte';
-  import DownloadIcon from '../icons/DownloadIcon.svelte';
-  import Dropdown from '../Dropdown.svelte';
-  import OverflowMenu from '../OverflowMenu.svelte';
-  import FileIcon from '../icons/FileIcon.svelte';
+    import ERC20Controls from './ERC20Controls.svelte';
+    import ERC721Controls from './ERC721Controls.svelte';
+    import CustomControls from './CustomControls.svelte';
+    import CopyIcon from '../icons/CopyIcon.svelte';
+    import CheckIcon from '../icons/CheckIcon.svelte';
+    import DownloadIcon from '../icons/DownloadIcon.svelte';
+    import Dropdown from '../Dropdown.svelte';
+    import OverflowMenu from '../OverflowMenu.svelte';
+    import FileIcon from '../icons/FileIcon.svelte';
 
-  import type { KindedOptions, Kind, Contract, OptionsErrorMessages } from '@openzeppelin/wizard-cairo';
-  import { ContractBuilder, buildGeneric, printContract, sanitizeKind, OptionsError } from '@openzeppelin/wizard-cairo';
-  import { postConfig } from '../post-config';
+    import type { KindedOptions, Kind, Contract, OptionsErrorMessages } from '@openzeppelin/wizard-cairo';
+    import { ContractBuilder, buildGeneric, printContract, sanitizeKind, OptionsError } from '@openzeppelin/wizard-cairo';
+    import { postConfig } from '../post-config';
 
-  import { saveAs } from 'file-saver';
-  import { injectHyperlinks } from './inject-hyperlinks';
+    import { saveAs } from 'file-saver';
+    import { injectHyperlinks } from './inject-hyperlinks';
 
-  const dispatch = createEventDispatcher();
+    const dispatch = createEventDispatcher();
 
-  let copyButtonText: string = 'Copy to clipboard';
-  export let tab: Kind = 'ERC20';
-  $: {
-    tab = sanitizeKind(tab);
-    dispatch('tab-change', tab);
-  };
+    let copyButtonText: string = 'Copy to clipboard';
+    export let tab: Kind = 'ERC20';
+    $: {
+      tab = sanitizeKind(tab);
+      dispatch('tab-change', tab);
+    };
 
-  let allOpts: { [k in Kind]?: Required<KindedOptions[k]> } = {};
-  let errors: { [k in Kind]?: OptionsErrorMessages } = {};
+    let allOpts: { [k in Kind]?: Required<KindedOptions[k]> } = {};
+    let errors: { [k in Kind]?: OptionsErrorMessages } = {};
 
-  let contract: Contract = new ContractBuilder();
+    let contract: Contract = new ContractBuilder();
 
-  $: opts = allOpts[tab];
+    $: opts = allOpts[tab];
 
-  $: {
-    if (opts) {
-      try {
-        contract = buildGeneric(opts);
-        errors[tab] = undefined;
-      } catch (e: unknown) {
-        if (e instanceof OptionsError) {
-          errors[tab] = e.messages;
-        } else {
-          throw e;
+    $: {
+      if (opts) {
+        try {
+          contract = buildGeneric(opts);
+          errors[tab] = undefined;
+        } catch (e: unknown) {
+          if (e instanceof OptionsError) {
+            errors[tab] = e.messages;
+          } else {
+            throw e;
+          }
         }
       }
     }
-  }
 
-  $: code = printContract(contract);
-  $: highlightedCode = injectHyperlinks(hljs.highlight(code, {language: 'cairo'}).value);
+    $: code = printContract(contract);
+    $: highlightedCode = injectHyperlinks(hljs.highlight(code, {language: 'cairo'}).value);
 
-  const language = 'cairo';
+    const language = 'cairo';
 
-  const copyHandler = async () => {
-    await navigator.clipboard.writeText(code);
-    copyButtonText = 'copied';
-    if (opts) {
-      await postConfig(opts, 'copy', language);
-    }
-    setTimeout(() => {
-      copyButtonText = 'copy to clipboard';
-    }, 1000);
-  };
+    const copyHandler = async () => {
+      await navigator.clipboard.writeText(code);
+      copyButtonText = 'copied';
+      if (opts) {
+        await postConfig(opts, 'copy', language);
+      }
+      setTimeout(() => {
+        copyButtonText = 'copy to clipboard';
+      }, 1000);
+    };
 
-  const downloadCairoHandler = async () => {
-    const blob = new Blob([code], { type: 'text/plain' });
-    if (opts) {
-      const name = ('name' in opts) ? opts.name : 'MyContract';
-      saveAs(blob, name + '.cairo');
-      await postConfig(opts, 'download-npm', language);
-    }
-  };
+    const downloadCairoHandler = async () => {
+      const blob = new Blob([code], { type: 'text/plain' });
+      if (opts) {
+        const name = ('name' in opts) ? opts.name : 'MyContract';
+        saveAs(blob, name + '.cairo');
+        await postConfig(opts, 'download-npm', language);
+      }
+    };
 
 </script>
 
 <div class="container flex flex-col gap-4 p-4">
-<div class="header flex flex-row justify-between">
-  <div class="tab overflow-hidden">
-    <OverflowMenu>
-      <button class:selected={tab === 'ERC20'} on:click={() => tab = 'ERC20'}>
-        ERC20
-      </button>
-      <button class:selected={tab === 'ERC721'} on:click={() => tab = 'ERC721'}>
-        ERC721
-      </button>
-      <button class:selected={tab === 'Custom'} on:click={() => tab = 'Custom'}>
-        Custom
-      </button>
-    </OverflowMenu>
-  </div>
+  <div class="header flex flex-row justify-between">
+    <div class="tab overflow-hidden">
+      <OverflowMenu>
+        <button class:selected={tab === 'ERC20'} on:click={() => tab = 'ERC20'}>
+          ERC20
+        </button>
+        <button class:selected={tab === 'ERC721'} on:click={() => tab = 'ERC721'}>
+          ERC721
+        </button>
+        <button class:selected={tab === 'Custom'} on:click={() => tab = 'Custom'}>
+          Custom
+        </button>
+      </OverflowMenu>
+    </div>
 
-  <div class="action flex flex-row gap-2 shrink-0">
-    <button class="action-button min-w-[165px]" on:click={copyHandler}>
-      {#if copyButtonText === 'copied'}
+    <div class="action flex flex-row gap-2 shrink-0">
+      <button class="action-button min-w-[165px]" on:click={copyHandler}>
+        {#if copyButtonText === 'copied'}
           <CheckIcon />
         {:else}
           <CopyIcon />
         {/if}
         {copyButtonText}
-    </button>
-
-    <Dropdown let:active>
-      <button class="action-button" class:active slot="button">
-        <DownloadIcon />
-        Download
       </button>
 
-      <button class="download-option" on:click={downloadCairoHandler}>
-        <FileIcon />
-        <div class="download-option-content">
-          <p>Single file</p>
-          <p>Requires installation of Python package (<code>openzeppelin-cairo-contracts</code>).</p>
-        </div>
-      </button>
-    </Dropdown>
-  </div>
-</div>
+      <Dropdown let:active>
+        <button class="action-button" class:active slot="button">
+          <DownloadIcon />
+          Download
+        </button>
 
-<div class="flex flex-row gap-4 grow">
-  <div class="controls w-64 flex flex-col shrink-0 justify-between">
-    <div class:hidden={tab !== 'ERC20'}>
-      <ERC20Controls bind:opts={allOpts.ERC20} errors={errors.ERC20} />
-    </div>
-    <div class:hidden={tab !== 'ERC721'}>
-      <ERC721Controls bind:opts={allOpts.ERC721} />
-    </div>
-    <div class:hidden={tab !== 'Custom'}>
-      <CustomControls bind:opts={allOpts.Custom} />
+        <button class="download-option" on:click={downloadCairoHandler}>
+          <FileIcon />
+          <div class="download-option-content">
+            <p>Single file</p>
+            <p>Requires installation of Python package (<code>openzeppelin-cairo-contracts</code>).</p>
+          </div>
+        </button>
+      </Dropdown>
     </div>
   </div>
-  <div class="output flex flex-col grow overflow-auto">
-  <pre class="flex flex-col grow basis-0 overflow-auto"><code class="hljs grow overflow-auto p-4">{@html highlightedCode}</code></pre>
+
+  <div class="flex flex-row gap-4 grow">
+    <div class="controls w-64 flex flex-col shrink-0 justify-between">
+      <div class:hidden={tab !== 'ERC20'}>
+        <ERC20Controls bind:opts={allOpts.ERC20} errors={errors.ERC20} />
+      </div>
+      <div class:hidden={tab !== 'ERC721'}>
+        <ERC721Controls bind:opts={allOpts.ERC721} />
+      </div>
+      <div class:hidden={tab !== 'Custom'}>
+        <CustomControls bind:opts={allOpts.Custom} />
+      </div>
+    </div>
+    <div class="output flex flex-col grow overflow-auto">
+    <pre class="flex flex-col grow basis-0 overflow-auto"><code class="hljs grow overflow-auto p-4">{@html highlightedCode}</code></pre>
+    </div>
   </div>
-</div>
 </div>
 
 <style lang="postcss">
-.container {
-  background-color: var(--gray-1);
-  border: 1px solid var(--gray-2);
-  border-radius: 10px;
-  min-width: 32rem;
-  min-height: 53rem;
-}
-
-.header {
-  font-size: var(--text-small);
-}
-
-.tab {
-  color: var(--gray-5);
-}
-
-.tab button, .action-button, :global(.overflow-btn) {
-  padding: var(--size-2) var(--size-3);
-  border-radius: 6px;
-  font-weight: bold;
-  cursor: pointer;
-}
-
-.tab button, :global(.overflow-btn) {
-  border: 0;
-  background-color: transparent;
-}
-
-.tab button:hover, :global(.overflow-btn):hover {
-  background-color: var(--gray-2);
-}
-
-.tab button.selected {
-  background-color: var(--cairo-orange-2);
-  color: white;
-  order: -1;
-}
-
-:global(.overflow-menu) button.selected {
-  order: unset;
-}
-
-.action-button {
-  background-color: var(--gray-1);
-  border: 1px solid var(--gray-3);
-  color: var(--gray-6);
-  cursor: pointer;
-
-  &:hover {
-    background-color: var(--gray-2);
-  }
-
-  &:active, &.active {
-    background-color: var(--gray-2);
-  }
-
-  :global(.icon) {
-    margin-right: var(--size-1);
-  }
-}
-
-.controls {
-  background-color: white;
-  padding: var(--size-4);
-}
-
-.controls, .output {
-  border-radius: 5px;
-  box-shadow: var(--shadow);
-}
-
-.controls-footer {
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-end;
-  color: var(--gray-5);
-  margin-top: var(--size-3);
-  padding: 0 var(--size-2);
-  font-size: var(--text-small);
-
-  & > * + * {
-    margin-left: var(--size-3);
-  }
-
-  :global(.icon) {
-    margin-right: 0.2em;
-    opacity: .8;
-  }
-
-  a {
-    color: inherit;
-    text-decoration: none;
-
-    &:hover {
-      color: var(--text-color);
-    }
-  }
-}
-
-.download-option {
-  display: flex;
-  padding: var(--size-2);
-  text-align: left;
-  background: none;
-  border: 1px solid transparent;
-  border-radius: 4px;
-  cursor: pointer;
-
-  :global(.icon) {
-    margin-top: var(--icon-adjust);
-  }
-
-  &:hover,
-  &:focus, {
+  .container {
     background-color: var(--gray-1);
-    border: 1px solid var(--gray-3);
+    border: 1px solid var(--gray-2);
+    border-radius: 10px;
+    min-width: 32rem;
+    min-height: 53rem;
   }
 
-  & div {
-    display: block;
-  }
-}
-
-.download-option-content {
-  margin-left: var(--size-3);
-  font-size: var(--text-small);
-
-  & > :first-child {
-    margin-bottom: var(--size-2);
-    color: var(--gray-6);
-    font-weight: bold;
+  .header {
+    font-size: var(--text-small);
   }
 
-  & > :not(:first-child) {
-    margin-top: var(--size-1);
+  .tab {
     color: var(--gray-5);
   }
-}
+
+  .tab button, .action-button, :global(.overflow-btn) {
+    padding: var(--size-2) var(--size-3);
+    border-radius: 6px;
+    font-weight: bold;
+    cursor: pointer;
+  }
+
+  .tab button, :global(.overflow-btn) {
+    border: 0;
+    background-color: transparent;
+  }
+
+  .tab button:hover, :global(.overflow-btn):hover {
+    background-color: var(--gray-2);
+  }
+
+  .tab button.selected {
+    background-color: var(--cairo-orange-2);
+    color: white;
+    order: -1;
+  }
+
+  :global(.overflow-menu) button.selected {
+    order: unset;
+  }
+
+  .action-button {
+    background-color: var(--gray-1);
+    border: 1px solid var(--gray-3);
+    color: var(--gray-6);
+    cursor: pointer;
+
+    &:hover {
+      background-color: var(--gray-2);
+    }
+
+    &:active, &.active {
+      background-color: var(--gray-2);
+    }
+
+    :global(.icon) {
+      margin-right: var(--size-1);
+    }
+  }
+
+  .controls {
+    background-color: white;
+    padding: var(--size-4);
+  }
+
+  .controls, .output {
+    border-radius: 5px;
+    box-shadow: var(--shadow);
+  }
+
+  .controls-footer {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    color: var(--gray-5);
+    margin-top: var(--size-3);
+    padding: 0 var(--size-2);
+    font-size: var(--text-small);
+
+    & > * + * {
+      margin-left: var(--size-3);
+    }
+
+    :global(.icon) {
+      margin-right: 0.2em;
+      opacity: .8;
+    }
+
+    a {
+      color: inherit;
+      text-decoration: none;
+
+      &:hover {
+        color: var(--text-color);
+      }
+    }
+  }
+
+  .download-option {
+    display: flex;
+    padding: var(--size-2);
+    text-align: left;
+    background: none;
+    border: 1px solid transparent;
+    border-radius: 4px;
+    cursor: pointer;
+
+    :global(.icon) {
+      margin-top: var(--icon-adjust);
+    }
+
+    &:hover,
+    &:focus, {
+      background-color: var(--gray-1);
+      border: 1px solid var(--gray-3);
+    }
+
+    & div {
+      display: block;
+    }
+  }
+
+  .download-option-content {
+    margin-left: var(--size-3);
+    font-size: var(--text-small);
+
+    & > :first-child {
+      margin-bottom: var(--size-2);
+      color: var(--gray-6);
+      font-weight: bold;
+    }
+
+    & > :not(:first-child) {
+      margin-top: var(--size-1);
+      color: var(--gray-5);
+    }
+  }
 
 </style>
