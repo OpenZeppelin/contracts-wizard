d2f678130f657387ecec5151be761ab5b94522c8
jveer634 <jaysparx9@gmail.com>
2022-09-06 01:30:46 +0530

    add: generic test cases

--- packages/core/src/build-generic.ts
+++ packages/core/src/build-generic.ts
@@ -1,38 +1,43 @@
-import { CustomOptions, buildCustom } from './custom';
-import { ERC20Options, buildERC20 } from './erc20';
-import { ERC721Options, buildERC721 } from './erc721';
-import { ERC1155Options, buildERC1155 } from './erc1155';
-import { GovernorOptions, buildGovernor } from './governor';
+import { CustomOptions, buildCustom } from "./custom";
+import { ERC20Options, buildERC20 } from "./erc20";
+import { ERC721Options, buildERC721 } from "./erc721";
+import { ERC777Options, buildERC777 } from "./erc777";
+import { ERC1155Options, buildERC1155 } from "./erc1155";
+import { GovernorOptions, buildGovernor } from "./governor";
 
 export interface KindedOptions {
-  ERC20:    { kind: 'ERC20' }    & ERC20Options;
-  ERC721:   { kind: 'ERC721' }   & ERC721Options;
-  ERC1155:  { kind: 'ERC1155' }  & ERC1155Options;
-  Governor: { kind: 'Governor' } & GovernorOptions;
-  Custom:  { kind: 'Custom' }  & CustomOptions;
+    ERC20: { kind: "ERC20" } & ERC20Options;
+    ERC721: { kind: "ERC721" } & ERC721Options;
+    ERC777: { kind: "ERC777" } & ERC777Options;
+    ERC1155: { kind: "ERC1155" } & ERC1155Options;
+    Governor: { kind: "Governor" } & GovernorOptions;
+    Custom: { kind: "Custom" } & CustomOptions;
 }
 
 export type GenericOptions = KindedOptions[keyof KindedOptions];
 
 export function buildGeneric(opts: GenericOptions) {
-  switch (opts.kind) {
-    case 'ERC20':
-      return buildERC20(opts);
+    switch (opts.kind) {
+        case "ERC20":
+            return buildERC20(opts);
 
-    case 'ERC721':
-      return buildERC721(opts);
+        case "ERC721":
+            return buildERC721(opts);
 
-    case 'ERC1155':
-      return buildERC1155(opts);
+        case "ERC777":
+            return buildERC777(opts);
 
-    case 'Governor':
-      return buildGovernor(opts);
+        case "ERC1155":
+            return buildERC1155(opts);
 
-    case 'Custom':
-      return buildCustom(opts);
+        case "Governor":
+            return buildGovernor(opts);
 
-    default:
-      const _: never = opts;
-      throw new Error('Unknown ERC');
-  }
+        case "Custom":
+            return buildCustom(opts);
+
+        default:
+            const _: never = opts;
+            throw new Error("Unknown ERC");
+    }
 }
--- packages/core/src/test.ts
+++ packages/core/src/test.ts
@@ -1,53 +1,66 @@
-import test from 'ava';
-import hre from 'hardhat';
-import path from 'path';
+import test from "ava";
+import hre from "hardhat";
+import path from "path";
 
-import { generateSources, writeGeneratedSources } from './generate/sources';
-import type { GenericOptions } from './build-generic';
-import { custom, erc1155, erc20, erc721, governor } from './api';
+import { generateSources, writeGeneratedSources } from "./generate/sources";
+import type { GenericOptions } from "./build-generic";
+import { custom, erc1155, erc20, erc777, erc721, governor } from "./api";
 
-test('result compiles', async t => {
-  const generatedSourcesPath = path.join(hre.config.paths.sources, 'generated');
-  await writeGeneratedSources(generatedSourcesPath, 'all');
+test("result compiles", async (t) => {
+    const generatedSourcesPath = path.join(
+        hre.config.paths.sources,
+        "generated"
+    );
+    await writeGeneratedSources(generatedSourcesPath, "all");
 
-  // We only want to check that contracts compile and we don't care about any
-  // of the outputs. Setting empty outputSelection causes compilation to go a
-  // lot faster and not run out of memory.
-  for (const { settings } of hre.config.solidity.compilers) {
-    settings.outputSelection = {};
-  }
+    // We only want to check that contracts compile and we don't care about any
+    // of the outputs. Setting empty outputSelection causes compilation to go a
+    // lot faster and not run out of memory.
+    for (const { settings } of hre.config.solidity.compilers) {
+        settings.outputSelection = {};
+    }
 
-  await hre.run('compile');
-  t.pass();
+    await hre.run("compile");
+    t.pass();
 });
 
 function isAccessControlRequired(opts: GenericOptions) {
-  switch(opts.kind) {
-    case 'ERC20':
-      return erc20.isAccessControlRequired(opts);
-    case 'ERC721':
-      return erc721.isAccessControlRequired(opts);
-    case 'ERC1155':
-      return erc1155.isAccessControlRequired(opts);
-    case 'Governor':
-      return governor.isAccessControlRequired(opts);
-    case 'Custom':
-      return custom.isAccessControlRequired(opts);
-    default:
-      throw new Error("No such kind");
-  }
+    switch (opts.kind) {
+        case "ERC20":
+            return erc20.isAccessControlRequired(opts);
+        case "ERC721":
+            return erc721.isAccessControlRequired(opts);
+        case "ERC777":
+            return erc777.isAccessControlRequired(opts);
+        case "ERC1155":
+            return erc1155.isAccessControlRequired(opts);
+        case "Governor":
+            return governor.isAccessControlRequired(opts);
+        case "Custom":
+            return custom.isAccessControlRequired(opts);
+        default:
+            throw new Error("No such kind");
+    }
 }
 
-test('is access control required', async t => {
-  for (const contract of generateSources('all')) {
-    const regexOwnable = /import.*Ownable(Upgradeable)?.sol.*/gm;
+test("is access control required", async (t) => {
+    for (const contract of generateSources("all")) {
+        const regexOwnable = /import.*Ownable(Upgradeable)?.sol.*/gm;
 
-    if (!contract.options.access) {
-      if (isAccessControlRequired(contract.options)) {
-        t.regex(contract.source, regexOwnable, JSON.stringify(contract.options));
-      } else {
-        t.notRegex(contract.source, regexOwnable, JSON.stringify(contract.options));
-      }
+        if (!contract.options.access) {
+            if (isAccessControlRequired(contract.options)) {
+                t.regex(
+                    contract.source,
+                    regexOwnable,
+                    JSON.stringify(contract.options)
+                );
+            } else {
+                t.notRegex(
+                    contract.source,
+                    regexOwnable,
+                    JSON.stringify(contract.options)
+                );
+            }
+        }
     }
-  }
 });
