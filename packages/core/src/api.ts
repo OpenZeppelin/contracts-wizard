5d2d5fdc0e951c43d361f1b0c274a92ed3339357
jveer634 <jaysparx9@gmail.com>
2022-09-06 01:05:25 +0530

    add: erc777 token and supporting changes

--- packages/core/src/api.ts
+++ packages/core/src/api.ts
@@ -1,56 +1,93 @@
-import type { CommonOptions } from './common-options';
-import { printERC20, defaults as erc20defaults, isAccessControlRequired as erc20IsAccessControlRequired, ERC20Options } from './erc20';
-import { printERC721, defaults as erc721defaults, isAccessControlRequired as erc721IsAccessControlRequired, ERC721Options } from './erc721';
-import { printERC1155, defaults as erc1155defaults, isAccessControlRequired as erc1155IsAccessControlRequired, ERC1155Options } from './erc1155';
-import { printGovernor, defaults as governorDefaults, isAccessControlRequired as governorIsAccessControlRequired, GovernorOptions } from './governor';
-import { printCustom, defaults as customDefaults, isAccessControlRequired as customIsAccessControlRequired, CustomOptions } from './custom';
+import type { CommonOptions } from "./common-options";
+import {
+    printERC20,
+    defaults as erc20defaults,
+    isAccessControlRequired as erc20IsAccessControlRequired,
+    ERC20Options,
+} from "./erc20";
+import {
+    printERC777,
+    defaults as erc777defaults,
+    isAccessControlRequired as erc777IsAccessControlRequired,
+    ERC777Options,
+} from "./erc777";
+import {
+    printERC721,
+    defaults as erc721defaults,
+    isAccessControlRequired as erc721IsAccessControlRequired,
+    ERC721Options,
+} from "./erc721";
+import {
+    printERC1155,
+    defaults as erc1155defaults,
+    isAccessControlRequired as erc1155IsAccessControlRequired,
+    ERC1155Options,
+} from "./erc1155";
+import {
+    printGovernor,
+    defaults as governorDefaults,
+    isAccessControlRequired as governorIsAccessControlRequired,
+    GovernorOptions,
+} from "./governor";
+import {
+    printCustom,
+    defaults as customDefaults,
+    isAccessControlRequired as customIsAccessControlRequired,
+    CustomOptions,
+} from "./custom";
 
 export interface WizardContractAPI<Options extends CommonOptions> {
-  /**
-   * Returns a string representation of a contract generated using the provided options. If opts is not provided, uses `defaults`.
-   */
-  print: (opts?: Options) => string,
-  
-  /**
-   * The default options that are used for `print`.
-   */
-  defaults: Required<Options>;
+    /**
+     * Returns a string representation of a contract generated using the provided options. If opts is not provided, uses `defaults`.
+     */
+    print: (opts?: Options) => string;
 
-  /**
-   * Whether any of the provided options require access control to be enabled. If this returns `true`, then calling `print` with the 
-   * same options would cause the `access` option to default to `'ownable'` if it was `undefined` or `false`. 
-   */
-  isAccessControlRequired: (opts: Partial<Options>) => boolean,
+    /**
+     * The default options that are used for `print`.
+     */
+    defaults: Required<Options>;
+
+    /**
+     * Whether any of the provided options require access control to be enabled. If this returns `true`, then calling `print` with the
+     * same options would cause the `access` option to default to `'ownable'` if it was `undefined` or `false`.
+     */
+    isAccessControlRequired: (opts: Partial<Options>) => boolean;
 }
 
 export type ERC20 = WizardContractAPI<ERC20Options>;
 export type ERC721 = WizardContractAPI<ERC721Options>;
+export type ERC777 = WizardContractAPI<ERC777Options>;
 export type ERC1155 = WizardContractAPI<ERC1155Options>;
 export type Governor = WizardContractAPI<GovernorOptions>;
 export type Custom = WizardContractAPI<CustomOptions>;
 
 export const erc20: ERC20 = {
-  print: printERC20,
-  defaults: erc20defaults,
-  isAccessControlRequired: erc20IsAccessControlRequired
-}
+    print: printERC20,
+    defaults: erc20defaults,
+    isAccessControlRequired: erc20IsAccessControlRequired,
+};
 export const erc721: ERC721 = {
-  print: printERC721,
-  defaults: erc721defaults,
-  isAccessControlRequired: erc721IsAccessControlRequired
-}
+    print: printERC721,
+    defaults: erc721defaults,
+    isAccessControlRequired: erc721IsAccessControlRequired,
+};
+export const erc777: ERC777 = {
+    print: printERC777,
+    defaults: erc777defaults,
+    isAccessControlRequired: erc777IsAccessControlRequired,
+};
 export const erc1155: ERC1155 = {
-  print: printERC1155,
-  defaults: erc1155defaults,
-  isAccessControlRequired: erc1155IsAccessControlRequired
-}
+    print: printERC1155,
+    defaults: erc1155defaults,
+    isAccessControlRequired: erc1155IsAccessControlRequired,
+};
 export const governor: Governor = {
-  print: printGovernor,
-  defaults: governorDefaults,
-  isAccessControlRequired: governorIsAccessControlRequired
-}
+    print: printGovernor,
+    defaults: governorDefaults,
+    isAccessControlRequired: governorIsAccessControlRequired,
+};
 export const custom: Custom = {
-  print: printCustom,
-  defaults: customDefaults,
-  isAccessControlRequired: customIsAccessControlRequired
-}
+    print: printCustom,
+    defaults: customDefaults,
+    isAccessControlRequired: customIsAccessControlRequired,
+};
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,23 +1,23 @@
-export type { GenericOptions, KindedOptions } from './build-generic';
-export { buildGeneric } from './build-generic';
+export type { GenericOptions, KindedOptions } from "./build-generic";
+export { buildGeneric } from "./build-generic";
 
-export type { Contract } from './contract';
-export { ContractBuilder } from './contract';
+export type { Contract } from "./contract";
+export { ContractBuilder } from "./contract";
 
-export { printContract } from './print';
-export { printContractVersioned } from './print-versioned';
+export { printContract } from "./print";
+export { printContractVersioned } from "./print-versioned";
 
-export type { Access } from './set-access-control';
-export type { Upgradeable } from './set-upgradeable';
-export type { Info } from './set-info';
+export type { Access } from "./set-access-control";
+export type { Upgradeable } from "./set-upgradeable";
+export type { Info } from "./set-info";
 
-export { premintPattern } from './erc20';
-export { defaults as infoDefaults } from './set-info';
+export { premintPattern } from "./erc20";
+export { defaults as infoDefaults } from "./set-info";
 
-export type { OptionsErrorMessages } from './error';
-export { OptionsError } from './error';
+export type { OptionsErrorMessages } from "./error";
+export { OptionsError } from "./error";
 
-export type { Kind } from './kind';
-export { sanitizeKind } from './kind';
+export type { Kind } from "./kind";
+export { sanitizeKind } from "./kind";
 
-export { erc20, erc721, erc1155, governor, custom } from './api';
+export { erc20, erc721, erc777, erc1155, governor, custom } from "./api";
