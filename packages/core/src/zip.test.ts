2d4b3d8a57dd7513412644a26ce97c9ed5dca5e1
Eric Lau <ericglau@outlook.com>
2022-10-20 16:53:36 -0400

    Add upgradeable test

--- packages/core/src/zip.test.ts
+++ packages/core/src/zip.test.ts
@@ -57,6 +57,47 @@ test('erc721 auto increment', t => {
   ]);
 });
 
+test('erc721 auto increment uups', t => {
+  const c = buildERC721({ name: 'MyToken', symbol: 'MTK', mintable: true, incremental: true, upgradeable: 'uups' });
+  const zip = zipContract(c);
+  const files = Object.values(zip.files).map(f => f.name).sort();
+
+  t.deepEqual(files, [
+    '@openzeppelin/',
+    '@openzeppelin/contracts-upgradeable/',
+    '@openzeppelin/contracts-upgradeable/README.md',
+    '@openzeppelin/contracts-upgradeable/access/',
+    '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol',
+    '@openzeppelin/contracts-upgradeable/interfaces/',
+    '@openzeppelin/contracts-upgradeable/interfaces/draft-IERC1822Upgradeable.sol',
+    '@openzeppelin/contracts-upgradeable/proxy/',
+    '@openzeppelin/contracts-upgradeable/proxy/ERC1967/',
+    '@openzeppelin/contracts-upgradeable/proxy/ERC1967/ERC1967UpgradeUpgradeable.sol',
+    '@openzeppelin/contracts-upgradeable/proxy/beacon/',
+    '@openzeppelin/contracts-upgradeable/proxy/beacon/IBeaconUpgradeable.sol',
+    '@openzeppelin/contracts-upgradeable/proxy/utils/',
+    '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol',
+    '@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol',
+    '@openzeppelin/contracts-upgradeable/token/',
+    '@openzeppelin/contracts-upgradeable/token/ERC721/',
+    '@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol',
+    '@openzeppelin/contracts-upgradeable/token/ERC721/IERC721ReceiverUpgradeable.sol',
+    '@openzeppelin/contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol',
+    '@openzeppelin/contracts-upgradeable/token/ERC721/extensions/',
+    '@openzeppelin/contracts-upgradeable/token/ERC721/extensions/IERC721MetadataUpgradeable.sol',
+    '@openzeppelin/contracts-upgradeable/utils/',
+    '@openzeppelin/contracts-upgradeable/utils/AddressUpgradeable.sol',
+    '@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol',
+    '@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol',
+    '@openzeppelin/contracts-upgradeable/utils/StorageSlotUpgradeable.sol',
+    '@openzeppelin/contracts-upgradeable/utils/StringsUpgradeable.sol',
+    '@openzeppelin/contracts-upgradeable/utils/introspection/',
+    '@openzeppelin/contracts-upgradeable/utils/introspection/ERC165Upgradeable.sol',
+    '@openzeppelin/contracts-upgradeable/utils/introspection/IERC165Upgradeable.sol',
+    'MyToken.sol',
+  ]);
+});
+
 test('can zip all combinations', t => {
   for (const { options } of generateSources('all')) {
     const c = buildGeneric(options);
