1c24258fa3095e945d6e022d38a15be484a21abb
Eric Lau <ericglau@outlook.com>
2022-10-20 15:29:51 -0400

    Include all imports in zip file

--- packages/core/src/zip.test.ts
+++ packages/core/src/zip.test.ts
@@ -2,6 +2,7 @@ import test from 'ava';
 
 import { zipContract } from './zip';
 import { buildERC20 } from './erc20';
+import { buildERC721 } from './erc721';
 import { generateSources } from './generate/sources';
 import { buildGeneric } from './build-generic';
 
@@ -22,6 +23,36 @@ test('erc20 basic', t => {
     '@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol',
     '@openzeppelin/contracts/utils/',
     '@openzeppelin/contracts/utils/Context.sol',
+    'MyToken.sol',
+  ]);
+});
+
+test('erc721 auto increment', t => {
+  const c = buildERC721({ name: 'MyToken', symbol: 'MTK', mintable: true, incremental: true });
+  const zip = zipContract(c);
+  const files = Object.values(zip.files).map(f => f.name).sort();
+
+  t.deepEqual(files, [
+    '@openzeppelin/',
+    '@openzeppelin/contracts/',
+    '@openzeppelin/contracts/README.md',
+    '@openzeppelin/contracts/access/',
+    '@openzeppelin/contracts/access/Ownable.sol',
+    '@openzeppelin/contracts/token/',
+    '@openzeppelin/contracts/token/ERC721/',
+    '@openzeppelin/contracts/token/ERC721/ERC721.sol',
+    '@openzeppelin/contracts/token/ERC721/IERC721.sol',
+    '@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol',
+    '@openzeppelin/contracts/token/ERC721/extensions/',
+    '@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol',
+    '@openzeppelin/contracts/utils/',
+    '@openzeppelin/contracts/utils/Address.sol',
+    '@openzeppelin/contracts/utils/Context.sol',
+    '@openzeppelin/contracts/utils/Counters.sol',
+    '@openzeppelin/contracts/utils/Strings.sol',
+    '@openzeppelin/contracts/utils/introspection/',
+    '@openzeppelin/contracts/utils/introspection/ERC165.sol',
+    '@openzeppelin/contracts/utils/introspection/IERC165.sol',
     'MyToken.sol',
   ]);
 });
