9356c16456650c674934b58b2fb70779efff5d1f
Eric Lau <ericglau@outlook.com>
2022-10-21 17:27:27 -0400

    Test actual dev package

--- packages/core/src/environments/zipHardhat.test.ts
+++ packages/core/src/environments/zipHardhat.test.ts
@@ -5,8 +5,13 @@ import { zipHardhat } from './zipHardhat';
 import { buildERC20 } from '../erc20';
 import { generateSources } from '../generate/sources';
 import { buildGeneric } from '../build-generic';
+import { promises as fs } from 'fs';
+import path from 'path';
+import os from 'os';
+import util from 'util';
+import child from "child_process";
 
-test('erc20 basic', t => {
+test('erc20 basic', async t => {
   const c = buildERC20({ name: 'MyToken', symbol: 'MTK' });
   const zip = zipHardhat(c);
   const files = Object.values(zip.files).map(f => f.name).sort();
@@ -25,6 +30,22 @@ test('erc20 basic', t => {
     'test/test.ts',
     'tsconfig.json',
   ]);
+
+  let tempFolder = await fs.mkdtemp(path.join(os.tmpdir(), 'openzeppelin-wizard-'));
+
+  const items = Object.values(zip.files);
+  for (const item of items) {
+    if (item.dir) {
+      await fs.mkdir(path.join(tempFolder, item.name));
+    } else {
+      await fs.writeFile(path.join(tempFolder, item.name), Buffer.from(await item?.async('arraybuffer')));
+    }
+  }
+
+  const exec = util.promisify(child.exec);
+  const result = await exec(`cd "${tempFolder}" && npm config set scripts-prepend-node-path auto && npm install && npm test && npx hardhat run scripts/deploy.ts`);
+  t.regex(result.stdout, /1 passing/);
+  t.regex(result.stdout, /deployed to/);
 });
 
 test('can zip all combinations', t => {
