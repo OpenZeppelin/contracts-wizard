f9d5b3b1d37344e35cacf197367acecd01c55bce
Eric Lau <ericglau@outlook.com>
2022-11-07 16:49:04 -0500

    Use snapshot for zip hardhat test, fix opts in test

--- packages/core/src/zip-hardhat.test.ts
+++ packages/core/src/zip-hardhat.test.ts
@@ -13,37 +13,51 @@ import util, { promisify } from 'util';
 import child from "child_process";
 import type { Contract } from './contract';
 import _rimraf from 'rimraf';
+import type { JSZipObject } from 'jszip';
+import type JSZip from 'jszip';
+import type { GenericOptions } from './build-generic';
 
 const rimraf = promisify(_rimraf);
 
 test.serial('erc20 basic', async t => {
-  const c = buildERC20({ name: 'MyToken', symbol: 'MTK' });
-  await run(c, t);
+  const opts: GenericOptions = { kind: 'ERC20', name: 'My Token', symbol: 'MTK' };
+  const c = buildERC20(opts);
+  await runTest(c, t, opts);
 });
 
 test.serial('erc721 upgradeable', async t => {
-  const c = buildERC721({ name: 'MyToken', symbol: 'MTK', upgradeable: 'uups' });
-  await run(c, t);
+  const opts: GenericOptions = { kind: 'ERC721', name: 'My Token', symbol: 'MTK', upgradeable: 'uups' };
+  const c = buildERC721(opts);
+  await runTest(c, t, opts);
 });
 
 test.serial('erc1155 basic', async t => {
-  const c = buildERC1155({ name: 'MyToken', uri: 'https://myuri/{id}'});
-  await run(c, t);
+  const opts: GenericOptions = { kind: 'ERC1155', name: 'My Token', uri: 'https://myuri/{id}' };
+  const c = buildERC1155(opts);
+  await runTest(c, t, opts);
 });
 
 test.serial('custom basic', async t => {
-  const c = buildCustom({ name: 'MyContract' });
-  await run(c, t);
+  const opts: GenericOptions = { kind: 'Custom', name: 'My Contract' };
+  const c = buildCustom(opts);
+  await runTest(c, t, opts);
 });
 
 test.serial('custom upgradeable', async t => {
-  const c = buildCustom({ name: 'MyContract', upgradeable: 'transparent' });
-  await run(c, t);
+  const opts: GenericOptions = { kind: 'Custom',  name: 'My Contract', upgradeable: 'transparent' };
+  const c = buildCustom(opts);
+  await runTest(c, t, opts);
 });
 
-async function run(c: Contract, t: ExecutionContext<unknown>) {
-  const zip = await zipHardhat(c);
+async function runTest(c: Contract, t: ExecutionContext<unknown>, opts: GenericOptions) {
+  const zip = await zipHardhat(c, opts);
 
+  assertLayout(zip, t, c);
+  await extractAndRunPackage(zip, t);
+  await assertContents(zip, c, t);
+}
+
+function assertLayout(zip: JSZip, t: ExecutionContext<unknown>, c: Contract) {
   const sorted = Object.values(zip.files).map(f => f.name).sort();
   t.deepEqual(sorted, [
     '.gitignore',
@@ -59,7 +73,9 @@ async function run(c: Contract, t: ExecutionContext<unknown>) {
     'test/test.ts',
     'tsconfig.json',
   ]);
+}
 
+async function extractAndRunPackage(zip: JSZip, t: ExecutionContext<unknown>) {
   const files = Object.values(zip.files);
 
   let tempFolder = await fs.mkdtemp(path.join(os.tmpdir(), 'openzeppelin-wizard-'));
@@ -69,7 +85,7 @@ async function run(c: Contract, t: ExecutionContext<unknown>) {
       if (item.dir) {
         await fs.mkdir(path.join(tempFolder, item.name));
       } else {
-        await fs.writeFile(path.join(tempFolder, item.name), Buffer.from(await item?.async('arraybuffer')));
+        await fs.writeFile(path.join(tempFolder, item.name), await asString(item));
       }
     }
 
@@ -80,4 +96,29 @@ async function run(c: Contract, t: ExecutionContext<unknown>) {
   } finally {
     await rimraf(tempFolder);
   }
+}
+
+async function assertContents(zip: JSZip, c: Contract, t: ExecutionContext<unknown>) {
+  const contentComparison = [
+    await getItemString(zip, `contracts/${c.name}.sol`),
+    await getItemString(zip, 'hardhat.config.ts'),
+    await getItemString(zip, 'package.json'),
+    await getItemString(zip, 'scripts/deploy.ts'),
+    await getItemString(zip, 'test/test.ts'),
+  ];
+
+  t.snapshot(contentComparison);
+}
+
+async function getItemString(zip: JSZip, key: string) {
+  const obj = zip.files[key];
+  if (obj !== undefined) {
+    return await asString(obj);
+  } else {
+    return '';
+  }
+}
+
+async function asString(item: JSZipObject) {
+  return Buffer.from(await item.async('arraybuffer')).toString();
 }
