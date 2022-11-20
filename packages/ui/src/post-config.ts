5846e9b9d89bf1c7758ae0b064c01cc4efde7446
Francisco Giordano <frangio.1@gmail.com>
2022-11-20 18:18:25 -0300

    Simplify code for postConfig

--- packages/ui/src/post-config.ts
+++ packages/ui/src/post-config.ts
@@ -4,7 +4,7 @@ import { v4 as uuid } from 'uuid';
 
 declare global {
   interface Window {
-    gtag(...args: unknown[]): void;
+    gtag?: (...args: unknown[]) => void;
   }
 }
 const instance = uuid();
@@ -13,16 +13,5 @@ export type Action = 'copy' | 'remix' | 'download-npm' | 'download-vendored' | '
 export type Language = 'solidity' | 'cairo';
 
 export async function postConfig(opts: Required<GenericOptions> | Required<CairoOptions>, action: Action, language: Language) {
-  sendAnalyticsToGTM(opts, action, language);
-}
-function sendAnalyticsToGTM(opts: any, action: Action, language: Language) {
-  var currentObject = opts;
-  
-  if (typeof window.gtag === 'undefined') {
-    return;
-  } 
-   
-  opts.action = action;
-  opts.language = language;
-  window.gtag("event", "wizard_action", opts);
+  window.gtag?.('event', 'wizard_action', { ...opts, action, language });
 }
