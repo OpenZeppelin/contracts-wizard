eb1c68b69180adb53f3ac9ab51f74b49b533fb2b
Francisco Giordano <fg@frang.io>
2023-05-08 18:15:49 -0300

    make regex matching of quotes balanced

--- packages/ui/src/utils/inject-hyperlinks.ts
+++ packages/ui/src/utils/inject-hyperlinks.ts
@@ -2,7 +2,7 @@ import { version as contractsVersion } from "@openzeppelin/contracts/package.jso
 
 export function injectHyperlinks(code: string) {
   // We are modifying HTML, so use HTML escaped chars. The pattern excludes paths that include /../ in the URL.
-  const importRegex = /(@openzeppelin\/)(contracts-upgradeable\/|contracts\/)((?:(?!\.\.)[^/]+\/)*?[^/]*?)(&quot;)/g
+  const importRegex = /&quot;(@openzeppelin\/)(contracts-upgradeable\/|contracts\/)((?:(?!\.\.)[^/]+\/)*?[^/]*?)&quot;/g
 
-  return code.replace(importRegex, `<a class="import-link" href="https://github.com/OpenZeppelin/openzeppelin-$2blob/v${contractsVersion}/contracts/$3" target="_blank" rel="noopener noreferrer">$1$2$3</a>$4`);
+  return code.replace(importRegex, `&quot;<a class="import-link" href="https://github.com/OpenZeppelin/openzeppelin-$2blob/v${contractsVersion}/contracts/$3" target="_blank" rel="noopener noreferrer">$1$2$3</a>&quot;`);
 }
