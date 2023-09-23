c35199e599e85838ac05b325dd3aedf906eb69c7
Francisco Giordano <fg@frang.io>
2023-09-23 00:13:13 -0300

    remove ui

--- packages/ui/src/cairo/highlightjs.ts
+++
@@ -1,7 +0,0 @@
-import hljs from 'highlight.js/lib/core';
-
-// @ts-ignore
-import hljsDefineCairo from 'highlightjs-cairo';
-hljsDefineCairo(hljs);
-
-export default hljs;
--- packages/ui/src/remix.ts
+++
@@ -1,8 +0,0 @@
-export function remixURL(code: string, upgradeable = false): URL {
-  const remix = new URL('https://remix.ethereum.org');
-  remix.searchParams.set('code', btoa(code).replace(/=*$/, ''));
-  if (upgradeable) {
-    remix.searchParams.set('deployProxy', upgradeable.toString());
-  }
-  return remix;
-}
