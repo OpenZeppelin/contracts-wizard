97e06c68f49002c14cbad1d91acc6f21a01d94d0
Eric Lau <ericglau@outlook.com>
2024-02-26 13:17:23 -0500

    Remove Cairo lang version (#337)

--- packages/core-cairo/src/utils/version.ts
+++ packages/core-cairo/src/utils/version.ts
@@ -8,8 +8,3 @@ export const contractsVersionTag = `v${contractsVersion}`;
  * Semantic version string representing of the minimum compatible version of Contracts to display in output.
  */
 export const compatibleContractsSemver = '^0.9.0';
-
-/**
- * Compatible Cairo lang version to display in output.
- */
-export const compatibleCairoLangSemver = '2.5.3';
