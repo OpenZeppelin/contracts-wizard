c15c401a355d36d6851f441d1c0831636aaa6f94
Eric Lau <ericglau@outlook.com>
2024-04-02 17:09:20 -0400

    Fix svelte check

--- packages/ui/src/cairo/App.svelte
+++ packages/ui/src/cairo/App.svelte
@@ -137,7 +137,7 @@
         <ERC721Controls bind:opts={allOpts.ERC721} errors={errors.ERC721}/>
       </div>
       <div class:hidden={tab !== 'ERC1155'}>
-        <ERC1155Controls bind:opts={allOpts.ERC1155} errors={errors.ERC1155}/>
+        <ERC1155Controls bind:opts={allOpts.ERC1155}/>
       </div>
       <div class:hidden={tab !== 'Custom'}>
         <CustomControls bind:opts={allOpts.Custom} errors={errors.Custom}/>
