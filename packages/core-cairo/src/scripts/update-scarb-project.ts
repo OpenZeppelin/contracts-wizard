a52f8bda52a6882e2a9fc6491da09cc14a45b18e
Eric Nordelo <eric.nordelo39@gmail.com>
2024-06-20 16:12:54 +0200

    feat: get version from version file

--- packages/core-cairo/src/scripts/update-scarb-project.ts
+++ packages/core-cairo/src/scripts/update-scarb-project.ts
@@ -2,9 +2,10 @@ import { promises as fs } from 'fs';
 import path from 'path';
 
 import { writeGeneratedSources } from '../generate/sources';
+import { contractsVersionTag, edition, cairoVersion, scarbVersion } from '../utils/version';
 
 export async function updateScarbProject() {
-  const generatedSourcesPath = path.join('test_project', `src`);
+  const generatedSourcesPath = path.join('test_project', 'src');
   await fs.rm(generatedSourcesPath, { force: true, recursive: true });
 
   // Generate the contracts source code
@@ -12,13 +13,28 @@ export async function updateScarbProject() {
 
   // Generate lib.cairo file
   writeLibCairo(contractNames);
+
+  // Update Scarb.toml
+  updateScarbToml();
 }
 
 async function writeLibCairo(contractNames: string[]) {
-  const libCairoPath = path.join('test_project/src', `lib.cairo`);
+  const libCairoPath = path.join('test_project/src', 'lib.cairo');
   const libCairo = contractNames.map(name => `pub mod ${name};\n`).join('');
   await fs.writeFile(libCairoPath, libCairo);
 }
 
+async function updateScarbToml() {
+  const scarbTomlPath = path.join('test_project', 'Scarb.toml');
+
+  let currentContent = await fs.readFile(scarbTomlPath, 'utf8');
+  let updatedContent = currentContent
+    .replace(/edition = "\w+"/, `edition = "${edition}"`)
+    .replace(/cairo-version = "\d+\.\d+\.\d+"/, `cairo-version = "${cairoVersion}"`)
+    .replace(/scarb-version = "\d+\.\d+\.\d+"/, `scarb-version = "${scarbVersion}"`)
+    .replace(/tag = "v\d+\.\d+\.\d+"/, `tag = "${contractsVersionTag}"`);
+
+  await fs.writeFile(scarbTomlPath, updatedContent, 'utf8');
+}
 
 updateScarbProject();
