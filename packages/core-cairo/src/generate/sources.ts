5ddbf4514e1fa97f39e305616a6661f6f539ee38
Eric Lau <ericglau@outlook.com>
2024-06-20 11:24:37 -0400

    Add Cairo compilation tests (#361)
    Co-authored-by: Eric Nordelo <eric.nordelo39@gmail.com>

--- packages/core-cairo/src/generate/sources.ts
+++ packages/core-cairo/src/generate/sources.ts
@@ -4,8 +4,9 @@ import crypto from 'crypto';
 
 import { generateERC20Options } from './erc20';
 import { generateERC721Options } from './erc721';
+import { generateERC1155Options } from './erc1155';
 import { generateCustomOptions } from './custom';
-import { buildGeneric, GenericOptions } from '../build-generic';
+import { buildGeneric, GenericOptions, KindedOptions } from '../build-generic';
 import { printContract } from '../print';
 import { OptionsError } from '../error';
 import { findCover } from '../utils/find-cover';
@@ -13,17 +14,31 @@ import type { Contract } from '../contract';
 
 type Subset = 'all' | 'minimal-cover';
 
-export function* generateOptions(): Generator<GenericOptions> {
-  for (const kindOpts of generateERC20Options()) {
-    yield { kind: 'ERC20', ...kindOpts };
+type Kind = keyof KindedOptions;
+
+export function* generateOptions(kind?: Kind): Generator<GenericOptions> {
+  if (!kind || kind === 'ERC20') {
+    for (const kindOpts of generateERC20Options()) {
+      yield { kind: 'ERC20', ...kindOpts };
+    }
   }
 
-  for (const kindOpts of generateERC721Options()) {
-    yield { kind: 'ERC721', ...kindOpts };
+  if (!kind || kind === 'ERC721') {
+    for (const kindOpts of generateERC721Options()) {
+      yield { kind: 'ERC721', ...kindOpts };
+    }
   }
 
-  for (const kindOpts of generateCustomOptions()) {
-    yield { kind: 'Custom', ...kindOpts };
+  if (!kind || kind === 'ERC1155') {
+    for (const kindOpts of generateERC1155Options()) {
+      yield { kind: 'ERC1155', ...kindOpts };
+    }
+  }
+
+  if (!kind || kind === 'Custom') {
+    for (const kindOpts of generateCustomOptions()) {
+      yield { kind: 'Custom', ...kindOpts };
+    }
   }
 }
 
@@ -37,10 +52,10 @@ interface GeneratedSource extends GeneratedContract {
   source: string;
 }
 
-function generateContractSubset(subset: Subset): GeneratedContract[] {
+function generateContractSubset(subset: Subset, kind?: Kind): GeneratedContract[] {
   const contracts = [];
 
-  for (const options of generateOptions()) {
+  for (const options of generateOptions(kind)) {
     const id = crypto
       .createHash('sha1')
       .update(JSON.stringify(options))
@@ -70,17 +85,26 @@ function generateContractSubset(subset: Subset): GeneratedContract[] {
   }
 }
 
-export function* generateSources(subset: Subset): Generator<GeneratedSource> {
-  for (const c of generateContractSubset(subset)) {
+export function* generateSources(subset: Subset, uniqueName?: boolean, kind?: Kind): Generator<GeneratedSource> {
+  let counter = 1;
+  for (const c of generateContractSubset(subset, kind)) {
+    if (uniqueName) {
+      c.contract.name = `Contract${counter++}`;
+    }
     const source = printContract(c.contract);
     yield { ...c, source };
   }
 }
 
-export async function writeGeneratedSources(dir: string, subset: Subset): Promise<void> {
+export async function writeGeneratedSources(dir: string, subset: Subset, uniqueName?: boolean, kind?: Kind): Promise<string[]> {
   await fs.mkdir(dir, { recursive: true });
+  let contractNames = [];
 
-  for (const { id, source } of generateSources(subset)) {
-    await fs.writeFile(path.format({ dir, name: id, ext: '.cairo' }), source);
+  for (const { id, contract, source } of generateSources(subset, uniqueName, kind)) {
+    const name = uniqueName ? contract.name : id;
+    await fs.writeFile(path.format({ dir, name, ext: '.cairo' }), source);
+    contractNames.push(name);
   }
+
+  return contractNames;
 }
--- packages/core-cairo/src/test.ts
+++ packages/core-cairo/src/test.ts
@@ -1,8 +1,42 @@
-import test from 'ava';
+import { promises as fs } from 'fs';
+import os from 'os';
+import _test, { TestFn, ExecutionContext } from 'ava';
+import path from 'path';
 
-import { generateSources } from './generate/sources';
-import type { GenericOptions } from './build-generic';
-import { custom, erc20, erc721 } from './api';
+import { generateSources, writeGeneratedSources } from './generate/sources';
+import type { GenericOptions, KindedOptions } from './build-generic';
+import { custom, erc20, erc721, erc1155 } from './api';
+
+
+interface Context {
+  generatedSourcesPath: string
+}
+
+const test = _test as TestFn<Context>;
+
+test.serial('erc20 result generated', async t => {
+  await testGenerate(t, 'ERC20');
+});
+
+test.serial('erc721 result generated', async t => {
+  await testGenerate(t, 'ERC721');
+});
+
+test.serial('erc1155 result generated', async t => {
+  await testGenerate(t, 'ERC1155');
+});
+
+test.serial('custom result generated', async t => {
+  await testGenerate(t, 'Custom');
+});
+
+async function testGenerate(t: ExecutionContext<Context>, kind: keyof KindedOptions) {
+  const generatedSourcesPath = path.join(os.tmpdir(), 'oz-wizard-cairo');
+  await fs.rm(generatedSourcesPath, { force: true, recursive: true });
+  await writeGeneratedSources(generatedSourcesPath, 'all', true, kind);
+
+  t.pass();
+}
 
 function isAccessControlRequired(opts: GenericOptions) {
   switch(opts.kind) {
@@ -10,6 +44,8 @@ function isAccessControlRequired(opts: GenericOptions) {
       return erc20.isAccessControlRequired(opts);
     case 'ERC721':
       return erc721.isAccessControlRequired(opts);
+    case 'ERC1155':
+      return erc1155.isAccessControlRequired(opts);
     case 'Custom':
       return custom.isAccessControlRequired(opts);
     default:
