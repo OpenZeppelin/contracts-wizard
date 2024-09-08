c778cb8795bebffb4d55bbc4c8e20eec560e163f
andrew <fleming-andrew@protonmail.com>
2024-09-07 22:46:20 -0500

    improve account build fns

--- packages/core-cairo/src/account.ts
+++ packages/core-cairo/src/account.ts
@@ -13,43 +13,43 @@ export const accountOptions = ['stark', 'eth'] as const;
 export type Account = typeof accountOptions[number];
 
 export const defaults: Required<AccountOptions> = {
-    name: 'MyAccount',
-    type: 'stark',
-    declare: true,
-    deploy: true,
-    pubkey: true,
-    access: 'account',
-    upgradeable: commonDefaults.upgradeable,
-    info: commonDefaults.info
-  } as const;
+  name: 'MyAccount',
+  type: 'stark',
+  declare: true,
+  deploy: true,
+  pubkey: true,
+  access: 'account',
+  upgradeable: commonDefaults.upgradeable,
+  info: commonDefaults.info
+} as const;
 
-  export function printAccount(opts: AccountOptions = defaults): string {
-    return printContract(buildAccount(opts));
-  }
+export function printAccount(opts: AccountOptions = defaults): string {
+  return printContract(buildAccount(opts));
+}
 
-  export interface AccountOptions extends CommonOptions {
-    name: string;
-    type: Account;
-    declare: boolean,
-    deploy: boolean,
-    pubkey: boolean
-  }
+export interface AccountOptions extends CommonOptions {
+  name: string;
+  type: Account;
+  declare?: boolean,
+  deploy?: boolean,
+  pubkey?: boolean
+}
 
-  function withDefaults(opts: AccountOptions): Required<AccountOptions> {
-    return {
-      ...opts,
-      ...withCommonDefaults(opts),
-      name: opts.name ?? defaults.name,
-      type: opts.type ?? defaults.type,
-      declare: opts.declare ?? defaults.declare,
-      deploy: opts.deploy ?? defaults.deploy,
-      pubkey: opts.pubkey ?? defaults.pubkey
-    };
+function withDefaults(opts: AccountOptions): Required<AccountOptions> {
+  return {
+    ...opts,
+    ...withCommonDefaults(opts),
+    name: opts.name ?? defaults.name,
+    type: opts.type ?? defaults.type,
+    declare: opts.declare ?? defaults.declare,
+    deploy: opts.deploy ?? defaults.deploy,
+    pubkey: opts.pubkey ?? defaults.pubkey
   }
+}
 
-  export function isAccessControlRequired(opts: Partial<AccountOptions>): boolean {
-    return opts.upgradeable === false;
-  }
+export function isAccessControlRequired(opts: Partial<AccountOptions>): boolean {
+  return opts.upgradeable === false;
+}
 
 export function buildAccount(opts: AccountOptions): Contract {
   const c = new ContractBuilder(opts.name);
@@ -65,23 +65,23 @@ export function buildAccount(opts: AccountOptions): Contract {
     c.addComponent(components.EthAccountComponent, [{ lit:'public_key' }], true);
   }
 
-    if (opts.declare && opts.deploy && opts.pubkey) {
-        addAccountMixin(c, opts.type);
-    } else {
-        addSRC6(c, opts.type)
+  if (opts.declare && opts.deploy && opts.pubkey) {
+    addAccountMixin(c, opts.type);
+  } else {
+    addSRC6(c, opts.type)
 
-        if (opts.declare) {
-            addDeclarer(c, opts.type)
-        }
-
-        if (opts.deploy) {
-            addDeployer(c, opts.type)
-        }
-
-        if (opts.pubkey) {
-            addPublicKey(c, opts.type)
-        }
+    if (opts.declare) {
+      addDeclarer(c, opts.type)
     }
+
+    if (opts.deploy) {
+        addDeployer(c, opts.type)
+    }
+
+    if (opts.pubkey) {
+        addPublicKey(c, opts.type)
+    }
+  }
 
   setAccountUpgradeable(c, allOpts.upgradeable, allOpts.type);
   setInfo(c, allOpts.info);
@@ -90,57 +90,58 @@ export function buildAccount(opts: AccountOptions): Contract {
 }
 
 function addSRC6(c: ContractBuilder, accountType: Account) {
-    let baseComponent = accountType === 'stark' ? 'AccountComponent' : 'EthAccountComponent';
-    let componentType = accountType === 'stark' ? components.AccountComponent : components.EthAccountComponent;
+  let baseComponent = accountType === 'stark' ? 'AccountComponent' : 'EthAccountComponent';
+  let componentType = accountType === 'stark' ? components.AccountComponent : components.EthAccountComponent;
 
-    c.addImplToComponent(componentType, {
-        name: 'SRC6Impl',
-        value: `${baseComponent}::SRC6Impl<ContractState>`,
-      });
-      c.addInterfaceFlag('ISRC5');
-      addSRC5Component(c);
+  c.addImplToComponent(componentType, {
+      name: 'SRC6Impl',
+      value: `${baseComponent}::SRC6Impl<ContractState>`,
+    });
+
+    c.addInterfaceFlag('ISRC5');
+    addSRC5Component(c);
 }
 
 function addDeclarer(c: ContractBuilder, accountType: Account) {
-    let baseComponent = accountType === 'stark' ? 'AccountComponent' : 'EthAccountComponent';
-    let componentType = accountType === 'stark' ? components.AccountComponent : components.EthAccountComponent;
+  let baseComponent = accountType === 'stark' ? 'AccountComponent' : 'EthAccountComponent';
+  let componentType = accountType === 'stark' ? components.AccountComponent : components.EthAccountComponent;
 
-    c.addImplToComponent(componentType, {
-        name: 'DeclarerImpl',
-        value: `${baseComponent}::DeclarerImpl<ContractState>`,
-      });
+  c.addImplToComponent(componentType, {
+      name: 'DeclarerImpl',
+      value: `${baseComponent}::DeclarerImpl<ContractState>`,
+    });
 }
 
 function addDeployer(c: ContractBuilder, accountType: Account) {
-    let baseComponent = accountType === 'stark' ? 'AccountComponent' : 'EthAccountComponent';
-    let componentType = accountType === 'stark' ? components.AccountComponent : components.EthAccountComponent;
+  let baseComponent = accountType === 'stark' ? 'AccountComponent' : 'EthAccountComponent';
+  let componentType = accountType === 'stark' ? components.AccountComponent : components.EthAccountComponent;
 
-    c.addImplToComponent(componentType, {
-        name: 'DeployerImpl',
-        value: `${baseComponent}::DeployerImpl<ContractState>`,
-      });
+  c.addImplToComponent(componentType, {
+      name: 'DeployerImpl',
+      value: `${baseComponent}::DeployerImpl<ContractState>`,
+    });
 }
 
 function addPublicKey(c: ContractBuilder, accountType: Account) {
-    let baseComponent = accountType === 'stark' ? 'AccountComponent' : 'EthAccountComponent';
-    let componentType = accountType === 'stark' ? components.AccountComponent : components.EthAccountComponent;
+  let baseComponent = accountType === 'stark' ? 'AccountComponent' : 'EthAccountComponent';
+  let componentType = accountType === 'stark' ? components.AccountComponent : components.EthAccountComponent;
 
-    c.addImplToComponent(componentType, {
-        name: 'PublicKeyImpl',
-        value: `${baseComponent}::PublicKeyImpl<ContractState>`,
-      });
+  c.addImplToComponent(componentType, {
+      name: 'PublicKeyImpl',
+      value: `${baseComponent}::PublicKeyImpl<ContractState>`,
+    });
 }
 
 function addAccountMixin(c: ContractBuilder, accountType: Account) {
-    let accountPrefix = accountType === 'stark' ? 'Account' : 'EthAccount';
-    let componentType = accountType === 'stark' ? components.AccountComponent : components.EthAccountComponent;
+  let accountPrefix = accountType === 'stark' ? 'Account' : 'EthAccount';
+  let componentType = accountType === 'stark' ? components.AccountComponent : components.EthAccountComponent;
 
-    c.addImplToComponent(componentType, {
-        name: `${accountPrefix}MixinImpl`,
-        value: `${accountPrefix}Component::${accountPrefix}MixinImpl<ContractState>`,
-      });
-      c.addInterfaceFlag('ISRC5');
-      addSRC5Component(c);
+  c.addImplToComponent(componentType, {
+      name: `${accountPrefix}MixinImpl`,
+      value: `${accountPrefix}Component::${accountPrefix}MixinImpl<ContractState>`,
+    });
+    c.addInterfaceFlag('ISRC5');
+    addSRC5Component(c);
 }
 
 const components = defineComponents( {
@@ -161,7 +162,7 @@ const components = defineComponents( {
     },
   },
   EthAccountComponent: {
-    path: 'openzeppelin::token::eth_account',
+    path: 'openzeppelin::account::eth_account',
     substorage: {
       name: 'eth_account',
       type: 'EthAccountComponent::Storage',
