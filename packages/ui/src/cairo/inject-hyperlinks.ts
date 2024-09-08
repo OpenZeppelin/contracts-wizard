c5c0a1793d05148b64ecec1deb253fe5bc517402
andrew <fleming-andrew@protonmail.com>
2024-09-08 12:49:20 -0500

    fix hyperlinks for cairo

--- packages/ui/src/cairo/inject-hyperlinks.ts
+++ packages/ui/src/cairo/inject-hyperlinks.ts
@@ -7,13 +7,31 @@ export function injectHyperlinks(code: string) {
   while (match != null) {
     const [line, libraryPrefix, libraryPath] = match;
     if (line !== undefined && libraryPrefix !== undefined && libraryPath !== undefined) {
-      const githubPrefix = `https://github.com/OpenZeppelin/cairo-contracts/blob/${contractsVersionTag}/src/`;
+      const githubPrefix = `https://github.com/OpenZeppelin/cairo-contracts/blob/${contractsVersionTag}/packages/`;
 
       let libraryPathSegments = libraryPath.split('::');
+      libraryPathSegments.splice(1, 0, 'src');
 
       // Remove the component name
-      if (libraryPathSegments.length > 0 && libraryPathSegments[libraryPathSegments.length - 1] !== 'interface') {
-        libraryPathSegments.pop();
+      let lastItem = libraryPathSegments[libraryPathSegments.length - 1];
+      if (libraryPathSegments.length > 0 && lastItem !== 'interface') {
+        // Replace component name with 'upgradeable'
+        if (lastItem === 'UpgradeableComponent') {
+          libraryPathSegments.splice(-1, 1, 'upgradeable')
+        } else {
+          libraryPathSegments.pop();
+        }
+      }
+
+      // Rebind variable after component name is removed
+      lastItem = libraryPathSegments[libraryPathSegments.length - 1];
+
+      // Duplicate file name as preceding directory in path
+      // unless coming from `notNestedPackages`
+      const notNestedPackages = ['security', 'introspection', 'upgrades'];
+      const packageName = libraryPathSegments[0];
+      if (lastItem !== undefined && packageName !== undefined && !notNestedPackages.includes(packageName) ) {
+        libraryPathSegments.splice(-1, 0, lastItem);
       }
 
       if (libraryPathSegments !== undefined && libraryPathSegments.length > 0) {
