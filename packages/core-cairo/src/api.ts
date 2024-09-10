ff0e9c0804b54ca251861a9a2e589ca2af0330eb
andrew <fleming-andrew@protonmail.com>
2024-09-09 23:48:37 -0500

    clean up api

--- packages/core-cairo/src/api.ts
+++ packages/core-cairo/src/api.ts
@@ -10,25 +10,7 @@ export interface WizardContractAPI<Options extends CommonOptions> {
    * Returns a string representation of a contract generated using the provided options. If opts is not provided, uses `defaults`.
    */
   print: (opts?: Options) => string,
-  
-  /**
-   * The default options that are used for `print`.
-   */
-  defaults: Required<Options>;
 
-  /**
-   * Whether any of the provided options require access control to be enabled. If this returns `true`, then calling `print` with the 
-   * same options would cause the `access` option to default to `'ownable'` if it was `undefined` or `false`. 
-   */
-   isAccessControlRequired: (opts: Partial<Options>) => boolean,
-}
-
-export interface WizardAccountAPI<Options extends CommonOptions> {
-  /**
-   * Returns a string representation of a contract generated using the provided options. If opts is not provided, uses `defaults`.
-   */
-  print: (opts?: Options) => string,
-  
   /**
    * The default options that are used for `print`.
    */
