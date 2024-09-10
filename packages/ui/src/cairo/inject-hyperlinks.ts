85200c61b8230eaa043e951519776f9b41003a76
andrew <fleming-andrew@protonmail.com>
2024-09-10 13:59:42 -0500

    remove duplication logic

--- packages/ui/src/cairo/inject-hyperlinks.ts
+++ packages/ui/src/cairo/inject-hyperlinks.ts
@@ -12,11 +12,7 @@ export function injectHyperlinks(code: string) {
       let libraryPathSegments = libraryPath.split('::');
       libraryPathSegments.splice(1, 0, 'src');
 
-      // Remove the component name
       removeComponentName(libraryPathSegments);
-
-      // Duplicate file name in path as preceding directory
-      duplicateFileAsDirectoryInPath(libraryPathSegments);
 
       if (libraryPathSegments !== undefined && libraryPathSegments.length > 0) {
         const replacedImportLine = `use<\/span> <a class="import-link" href='${githubPrefix}${libraryPathSegments.join('/')}.cairo' target='_blank' rel='noopener noreferrer'>${libraryPrefix}::${libraryPath}</a>;`;
@@ -30,23 +26,12 @@ export function injectHyperlinks(code: string) {
 
 function removeComponentName(libraryPathSegments: Array<string>) {
   const lastItem = libraryPathSegments[libraryPathSegments.length - 1];
-  if (libraryPathSegments.length > 0 && lastItem !== 'interface') {
+  if (lastItem !== 'interface') {
     if (lastItem === 'UpgradeableComponent') {
       // Replace component name with 'upgradeable'
       libraryPathSegments.splice(-1, 1, 'upgradeable');
     } else {
       libraryPathSegments.pop();
     }
-  }
-}
-
-/// Duplicate file name as preceding directory in path unless
-/// package name (`libraryPathSegments[0]`) is in `ignoredPackages`.
-function duplicateFileAsDirectoryInPath(libraryPathSegments: Array<string>) {
-  const ignoredPackages = ['security', 'introspection', 'upgrades'];
-  const packageName = libraryPathSegments[0];
-  const lastItem = libraryPathSegments[libraryPathSegments.length - 1];
-  if (lastItem !== undefined && packageName !== undefined && !ignoredPackages.includes(packageName) ) {
-    libraryPathSegments.splice(-1, 0, lastItem);
   }
 }
