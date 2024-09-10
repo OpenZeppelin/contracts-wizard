05329805bf7d758ef5cb4d990814c235fadebd87
Andrew Fleming <fleming.andrew@protonmail.com>
2024-09-09 22:38:38 -0400

    Bump cairo lib to v0.16.0 (#384)

--- packages/core-cairo/src/utils/version.ts
+++ packages/core-cairo/src/utils/version.ts
@@ -1,17 +1,17 @@
 /**
  * The actual latest version to use in links.
  */
-export const contractsVersion = '0.15.0';
+export const contractsVersion = '0.16.0';
 export const contractsVersionTag = `v${contractsVersion}`;
 
 /**
  * Cairo compiler versions.
  */
 export const edition = '2023_11';
-export const cairoVersion = '2.7.0';
-export const scarbVersion = '2.7.0';
+export const cairoVersion = '2.8.0';
+export const scarbVersion = '2.8.0';
 
 /**
  * Semantic version string representing of the minimum compatible version of Contracts to display in output.
  */
-export const compatibleContractsSemver = '^0.15.0';
+export const compatibleContractsSemver = '^0.16.0';
