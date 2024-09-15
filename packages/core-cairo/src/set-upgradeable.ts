b781354cdec9fbaf38fe7754f705f618ce91f72e
andrew <fleming-andrew@protonmail.com>
2024-09-15 10:46:22 -0500

    abstract upgradeable base logic

--- packages/core-cairo/src/set-upgradeable.ts
+++ packages/core-cairo/src/set-upgradeable.ts
@@ -9,7 +9,7 @@ export const upgradeableOptions = [false, true] as const;
 
 export type Upgradeable = typeof upgradeableOptions[number];
 
-export function setUpgradeable(c: ContractBuilder, upgradeable: Upgradeable, access: Access) {
+function setUpgradeableBase(c: ContractBuilder, upgradeable: Upgradeable): BaseImplementedTrait | undefined {
   if (upgradeable === false) {
     return;
   }
@@ -29,34 +29,24 @@ export function setUpgradeable(c: ContractBuilder, upgradeable: Upgradeable, acc
     ],
   };
   c.addImplementedTrait(t);
-  requireAccessControl(c, t, functions.upgrade, access, 'UPGRADER', 'upgrader');
+  return t;
+}
+
+export function setUpgradeable(c: ContractBuilder, upgradeable: Upgradeable, access: Access) {
+  const trait = setUpgradeableBase(c, upgradeable);
+  if (trait !== undefined) {
+    requireAccessControl(c, trait, functions.upgrade, access, 'UPGRADER', 'upgrader');
+  }
 }
 
 export function setAccountUpgradeable(c: ContractBuilder, upgradeable: Upgradeable, type: Account) {
-  if (upgradeable === false) {
-    return;
-  }
-
-  c.upgradeable = true;
-
-  c.addComponent(components.UpgradeableComponent, [], false);
-
-  c.addStandaloneImport('openzeppelin::upgrades::interface::IUpgradeable');
-  c.addStandaloneImport('starknet::ClassHash');
-
-  const t: BaseImplementedTrait = {
-    name: 'UpgradeableImpl',
-    of: 'IUpgradeable<ContractState>',
-    tags: [
-      'abi(embed_v0)'
-    ],
-  };
-  c.addImplementedTrait(t);
-
-  if (type === 'stark')
-    c.addFunctionCodeBefore(t, functions.upgrade, 'self.account.assert_only_self()');
-  else {
-    c.addFunctionCodeBefore(t, functions.upgrade, 'self.eth_account.assert_only_self()');
+  const trait = setUpgradeableBase(c, upgradeable);
+  if (trait !== undefined) {
+    if (type === 'stark') {
+      c.addFunctionCodeBefore(trait, functions.upgrade, 'self.account.assert_only_self()');
+    } else if (type === 'eth') {
+      c.addFunctionCodeBefore(trait, functions.upgrade, 'self.eth_account.assert_only_self()');
+    }
   }
 }
 
