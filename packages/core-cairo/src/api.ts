371cf55760c2260b366b210ff3abd20328e4dbfe
andrew <fleming-andrew@protonmail.com>
2024-09-17 17:11:56 -0500

    remove commas

--- packages/core-cairo/src/api.ts
+++ packages/core-cairo/src/api.ts
@@ -9,7 +9,7 @@ export interface WizardAccountAPI<Options extends CommonOptions>{
   /**
    * Returns a string representation of a contract generated using the provided options. If opts is not provided, uses `defaults`.
    */
-  print: (opts?: Options) => string,
+  print: (opts?: Options) => string;
 
   /**
    * The default options that are used for `print`.
@@ -21,7 +21,7 @@ export interface WizardContractAPI<Options extends CommonContractOptions> {
   /**
    * Returns a string representation of a contract generated using the provided options. If opts is not provided, uses `defaults`.
    */
-  print: (opts?: Options) => string,
+  print: (opts?: Options) => string;
 
   /**
    * The default options that are used for `print`.
@@ -32,7 +32,7 @@ export interface WizardContractAPI<Options extends CommonContractOptions> {
    * Whether any of the provided options require access control to be enabled. If this returns `true`, then calling `print` with the 
    * same options would cause the `access` option to default to `'ownable'` if it was `undefined` or `false`.
    */
-   isAccessControlRequired: (opts: Partial<Options>) => boolean,
+   isAccessControlRequired: (opts: Partial<Options>) => boolean;
 }
 
 export type ERC20 = WizardContractAPI<ERC20Options>;
