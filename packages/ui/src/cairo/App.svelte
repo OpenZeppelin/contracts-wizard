dcec04f4c3036db76ce2513e3dc4803975a33575
andrew <fleming-andrew@protonmail.com>
2024-09-17 17:57:27 -0500

    fix printContract

--- packages/ui/src/cairo/App.svelte
+++ packages/ui/src/cairo/App.svelte
@@ -73,7 +73,7 @@
       }
     }
 
-    $: code = printContract(contract, { isAccount: opts?.kind === 'Account' });
+    $: code = printContract(contract);
     $: highlightedCode = injectHyperlinks(hljs.highlight(code, {language: 'cairo'}).value);
 
     const language = 'cairo';
