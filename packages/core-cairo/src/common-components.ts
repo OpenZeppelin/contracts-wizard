f8fb22f2b00553ef56fdf359e85e0b7e930d306a
Andrew Fleming <fleming-andrew@protonmail.com>
2024-10-22 08:52:53 -0400

    Bump cairo to v0.18.0 (#399)
    Co-authored-by: Eric Lau <ericglau@outlook.com>
    Co-authored-by: Eric Nordelo <eric.nordelo39@gmail.com>

--- packages/core-cairo/src/common-components.ts
+++ packages/core-cairo/src/common-components.ts
@@ -1,5 +1,8 @@
-import type { ContractBuilder } from "./contract";
+import type { BaseImplementedTrait, ContractBuilder } from './contract';
 import { defineComponents } from "./utils/define-components";
+
+export const tokenTypes = ['ERC20', 'ERC721'] as const;
+export type Token = typeof tokenTypes[number];
 
 const components = defineComponents( {
   SRC5Component: {
@@ -14,6 +17,41 @@ const components = defineComponents( {
     },
     impls: [],
   },
+
+  VotesComponent: {
+    path: 'openzeppelin::governance::votes',
+    substorage: {
+      name: 'votes',
+      type: 'VotesComponent::Storage',
+    },
+    event: {
+      name: 'VotesEvent',
+      type: 'VotesComponent::Event',
+    },
+    impls: [],
+    internalImpl: {
+      name: 'VotesInternalImpl',
+      value: 'VotesComponent::InternalImpl<ContractState>',
+    },
+  },
+
+  NoncesComponent: {
+    path: 'openzeppelin::utils::cryptography::nonces',
+    substorage: {
+      name: 'nonces',
+      type: 'NoncesComponent::Storage',
+    },
+    event: {
+      name: 'NoncesEvent',
+      type: 'NoncesComponent::Event',
+    },
+    impls: [
+      {
+        name: 'NoncesImpl',
+        value: 'NoncesComponent::NoncesImpl<ContractState>',
+      },
+    ],
+  },
 })
 
 export function addSRC5Component(c: ContractBuilder) {
@@ -26,4 +64,40 @@ export function addSRC5Component(c: ContractBuilder) {
     });
     c.addInterfaceFlag('ISRC5');
   }
+}
+
+export function addVotesComponent(c: ContractBuilder, name: string, version: string) {
+  c.addStandaloneImport('openzeppelin::utils::cryptography::snip12::SNIP12Metadata');
+  c.addComponent(components.NoncesComponent, [], false);
+  c.addComponent(components.VotesComponent, [], false);
+  c.addImplToComponent(components.VotesComponent, {
+    name: 'VotesImpl',
+    value: `VotesComponent::VotesImpl<ContractState>`,
+  });
+
+  const SNIP12Metadata: BaseImplementedTrait = {
+    name: 'SNIP12MetadataImpl',
+    of: 'SNIP12Metadata',
+    tags: [],
+    priority: 0,
+  };
+  c.addImplementedTrait(SNIP12Metadata);
+
+  c.addFunction(SNIP12Metadata, {
+    name: 'name',
+    args: [],
+    returns: 'felt252',
+    code: [
+      `'${name}'`,
+    ],
+  });
+
+  c.addFunction(SNIP12Metadata, {
+    name: 'version',
+    args: [],
+    returns: 'felt252',
+    code: [
+      `'${version}'`,
+    ],
+  });
 }
--- packages/core-cairo/src/erc721.test.ts
+++ packages/core-cairo/src/erc721.test.ts
@@ -3,13 +3,16 @@ import test from 'ava';
 import { buildERC721, ERC721Options } from './erc721';
 import { printContract } from './print';
 
-import { erc721 } from '.';
+import { erc721, OptionsError } from '.';
 
 const allFeaturesON: Partial<ERC721Options> = {
   mintable: true,
   burnable: true,
   pausable: true,
   enumerable: true,
+  votes: true,
+  appName: 'MY_DAPP_NAME',
+  appVersion: 'MY_DAPP_VERSION',
   upgradeable: true
 } as const;
 
@@ -75,6 +78,54 @@ testERC721('mintable + roles', {
 
 testERC721('full non-upgradeable', {
   ...allFeaturesON,
+  upgradeable: false,
+});
+
+testERC721('erc721 votes', {
+  votes: true,
+  appName: 'MY_DAPP_NAME',
+});
+
+testERC721('erc721 votes, version', {
+  votes: true,
+  appName: 'MY_DAPP_NAME',
+  appVersion: 'MY_DAPP_VERSION',
+});
+
+test('erc721 votes, no name', async t => {
+  let error = t.throws(() => buildERC721({
+    name: 'MyToken',
+    symbol: 'MTK',
+    votes: true,
+  }));
+  t.is((error as OptionsError).messages.appName, 'Application Name is required when Votes are enabled');
+});
+
+test('erc721 votes, no version', async t => {
+  let error = t.throws(() => buildERC721({
+    name: 'MyToken',
+    symbol: 'MTK',
+    votes: true,
+    appName: 'MY_DAPP_NAME',
+    appVersion: ''
+  }));
+  t.is((error as OptionsError).messages.appVersion, 'Application Version is required when Votes are enabled');
+});
+
+test('erc721 votes, empty version', async t => {
+  let error = t.throws(() => buildERC721({
+    name: 'MyToken',
+    symbol: 'MTK',
+    votes: true,
+    appName: 'MY_DAPP_NAME',
+    appVersion: '', // avoids default value of v1
+  }));
+  t.is((error as OptionsError).messages.appVersion, 'Application Version is required when Votes are enabled');
+});
+
+testERC721('erc721 votes, non-upgradeable', {
+  votes: true,
+  appName: 'MY_DAPP_NAME',
   upgradeable: false,
 });
 
