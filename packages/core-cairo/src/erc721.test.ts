54110a020b3a7a4f6680e726e5b7ab2ff0bd3019
immrsd <immrsd.eth@gmail.com>
2024-12-02 14:32:16 +0100

    Refactor ERC721 tests

--- packages/core-cairo/src/erc721.test.ts
+++ packages/core-cairo/src/erc721.test.ts
@@ -2,25 +2,35 @@ import test from 'ava';
 
 import { buildERC721, ERC721Options } from './erc721';
 import { printContract } from './print';
+import { royaltyInfoOptions } from './set-royalty-info';
 
 import { erc721, OptionsError } from '.';
+
+const NAME = 'MyToken';
+const CUSTOM_NAME = 'CustomToken';
+const SYMBOL = 'MTK';
+const CUSTOM_SYMBOL = 'CTK';
+const APP_NAME = 'MY_DAPP_NAME';
+const APP_VERSION = 'MY_DAPP_VERSION';
+const BASE_URI = 'https://gateway.pinata.cloud/ipfs/QmcP9hxrnC1T5ATPmq2saFeAM1ypFX9BnAswCdHB9JCjLA/';
 
 const allFeaturesON: Partial<ERC721Options> = {
   mintable: true,
   burnable: true,
   pausable: true,
   enumerable: true,
+  royaltyInfo: royaltyInfoOptions.enabledDefault,
   votes: true,
-  appName: 'MY_DAPP_NAME',
-  appVersion: 'MY_DAPP_VERSION',
+  appName: APP_NAME,
+  appVersion: APP_VERSION,
   upgradeable: true
 } as const;
 
 function testERC721(title: string, opts: Partial<ERC721Options>) {
   test(title, t => {
     const c = buildERC721({
-      name: 'MyToken',
-      symbol: 'MTK',
+      name: NAME,
+      symbol: SYMBOL,
       ...opts,
     });
     t.snapshot(printContract(c));
@@ -33,8 +43,8 @@ function testERC721(title: string, opts: Partial<ERC721Options>) {
  function testAPIEquivalence(title: string, opts?: ERC721Options) {
   test(title, t => {
     t.is(erc721.print(opts), printContract(buildERC721({
-      name: 'MyToken',
-      symbol: 'MTK',
+      name: NAME,
+      symbol: SYMBOL,
       ...opts,
     })));
   });
@@ -47,7 +57,7 @@ testERC721('basic non-upgradeable', {
 testERC721('basic', {});
 
 testERC721('base uri', {
-  baseUri: 'https://gateway.pinata.cloud/ipfs/QmcP9hxrnC1T5ATPmq2saFeAM1ypFX9BnAswCdHB9JCjLA/',
+  baseUri: BASE_URI,
 });
 
 testERC721('burnable', {
@@ -107,19 +117,19 @@ testERC721('full non-upgradeable', {
 
 testERC721('erc721 votes', {
   votes: true,
-  appName: 'MY_DAPP_NAME',
+  appName: APP_NAME,
 });
 
 testERC721('erc721 votes, version', {
   votes: true,
-  appName: 'MY_DAPP_NAME',
-  appVersion: 'MY_DAPP_VERSION',
+  appName: APP_NAME,
+  appVersion: APP_VERSION,
 });
 
 test('erc721 votes, no name', async t => {
   let error = t.throws(() => buildERC721({
-    name: 'MyToken',
-    symbol: 'MTK',
+    name: NAME,
+    symbol: SYMBOL,
     votes: true,
   }));
   t.is((error as OptionsError).messages.appName, 'Application Name is required when Votes are enabled');
@@ -127,10 +137,10 @@ test('erc721 votes, no name', async t => {
 
 test('erc721 votes, no version', async t => {
   let error = t.throws(() => buildERC721({
-    name: 'MyToken',
-    symbol: 'MTK',
+    name: NAME,
+    symbol: SYMBOL,
     votes: true,
-    appName: 'MY_DAPP_NAME',
+    appName: APP_NAME,
     appVersion: ''
   }));
   t.is((error as OptionsError).messages.appVersion, 'Application Version is required when Votes are enabled');
@@ -138,10 +148,10 @@ test('erc721 votes, no version', async t => {
 
 test('erc721 votes, empty version', async t => {
   let error = t.throws(() => buildERC721({
-    name: 'MyToken',
-    symbol: 'MTK',
+    name: NAME,
+    symbol: SYMBOL,
     votes: true,
-    appName: 'MY_DAPP_NAME',
+    appName: APP_NAME,
     appVersion: '', // avoids default value of v1
   }));
   t.is((error as OptionsError).messages.appVersion, 'Application Version is required when Votes are enabled');
@@ -149,7 +159,7 @@ test('erc721 votes, empty version', async t => {
 
 testERC721('erc721 votes, non-upgradeable', {
   votes: true,
-  appName: 'MY_DAPP_NAME',
+  appName: APP_NAME,
   upgradeable: false,
 });
 
@@ -157,12 +167,12 @@ testERC721('full upgradeable', allFeaturesON);
 
 testAPIEquivalence('API default');
 
-testAPIEquivalence('API basic', { name: 'CustomToken', symbol: 'CTK' });
+testAPIEquivalence('API basic', { name: CUSTOM_NAME, symbol: CUSTOM_SYMBOL });
 
 testAPIEquivalence('API full upgradeable', {
   ...allFeaturesON,
-  name: 'CustomToken',
-  symbol: 'CTK'
+  name: CUSTOM_NAME,
+  symbol: CUSTOM_SYMBOL
 });
 
 test('API assert defaults', async t => {
@@ -173,6 +183,9 @@ test('API isAccessControlRequired', async t => {
   t.is(erc721.isAccessControlRequired({ mintable: true }), true);
   t.is(erc721.isAccessControlRequired({ pausable: true }), true);
   t.is(erc721.isAccessControlRequired({ upgradeable: true }), true);
+  t.is(erc721.isAccessControlRequired({ royaltyInfo: royaltyInfoOptions.enabledDefault }), true);
+  t.is(erc721.isAccessControlRequired({ royaltyInfo: royaltyInfoOptions.enabledCustom }), true);
+  t.is(erc721.isAccessControlRequired({ royaltyInfo: royaltyInfoOptions.disabled }), false);
   t.is(erc721.isAccessControlRequired({ burnable: true }), false);
   t.is(erc721.isAccessControlRequired({ enumerable: true }), false);
 });
