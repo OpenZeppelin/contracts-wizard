a2b498fe5a77dccf71a870790220ec1dae35fcf4
immrsd <immrsd.eth@gmail.com>
2025-01-28 07:28:35 +0100

    Add Multisig UI controls

---
+++ packages/ui/src/cairo/MultisigControls.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import HelpTooltip from '../HelpTooltip.svelte';
+  import UpgradeabilityField from './UpgradeabilityField.svelte';
+
+  import type { KindedOptions, OptionsErrorMessages } from '@openzeppelin/wizard-cairo';
+  import { multisig, infoDefaults } from '@openzeppelin/wizard-cairo';
+
+  import InfoSection from './InfoSection.svelte';
+  import { error } from '../error-tooltip';
+
+  const defaults = multisig.defaults;
+
+  export const opts: Required<KindedOptions['Multisig']> = {
+    kind: 'Multisig',
+    ...defaults,
+    info: { ...infoDefaults }, // create new object since Info is nested
+  };
+
+  export let errors: undefined | OptionsErrorMessages;
+</script>
+
+<section class="controls-section">
+  <h1>Settings</h1>
+  <label class="labeled-input">
+    <span class="flex justify-between pr-2">
+      Quorum
+      <HelpTooltip link="https://docs.openzeppelin.com/contracts-cairo/governance/multisig#signer_management">
+        The minimal number of confirmations required to approve a transaction.
+      </HelpTooltip>
+    </span>
+    <input bind:value={opts.quorum} placeholder='' use:error={errors?.quorum}>
+  </label>
+</section>
+
+<section class="controls-section">
+  <h1>Features</h1>
+  <div class="checkbox-group">
+    <UpgradeabilityField bind:upgradeable={opts.upgradeable} />
+  </div>
+</section>
+
+<InfoSection bind:info={opts.info} />
