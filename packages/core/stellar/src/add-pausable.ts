ac714d01d9920e648dc6a790da74dacbcd6c5ec9
Eric Lau <ericglau@outlook.com>
2025-02-07 11:18:30 -0500

    Import pausable

--- packages/core/stellar/src/add-pausable.ts
+++ packages/core/stellar/src/add-pausable.ts
@@ -4,6 +4,9 @@ import { Access, requireAccessControl } from './set-access-control';
 import { defineFunctions } from './utils/define-functions';
 
 export function addPausable(c: ContractBuilder, name: string, access: Access) {
+  c.addUseClause('openzeppelin_pausable', 'self as pausable');
+  c.addUseClause('openzeppelin_pausable', 'Pausable');
+
   const pausableTrait = {
     name: 'Pausable',
     for: name,
