cf6731c3e41c114b3a0b603723e6a80078d64b8a
Nenad <xinef.it@gmail.com>
2025-02-13 13:19:30 +0100

    fix: comment out/remove erc20metadata logic

--- packages/ui/src/stylus/inject-hyperlinks.ts
+++ packages/ui/src/stylus/inject-hyperlinks.ts
@@ -40,7 +40,7 @@ export function injectHyperlinks(code: string) {
 }
 
 const moduleMappings: { [key: string]: string } = {
-  'Erc20Metadata': 'token/erc20/extensions/metadata.rs',
+  // 'Erc20Metadata': 'token/erc20/extensions/metadata.rs',
   'Ownable': 'access/ownable.rs',
   'IErc165': 'utils/introspection/erc165.rs',
 } as const;
