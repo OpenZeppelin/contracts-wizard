86c65dc4dc2c2dc48b410f407bd0495d07526b22
CoveMB <bmarquiscom@gmail.com>
2025-02-21 15:12:36 -0500

    add lint note in readme

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,7 +15,7 @@ export default typescriptEslint.config(
     },
     rules: {
       '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
-      '@typescript-eslint/no-non-null-assertion': 'warn',
+      '@typescript-eslint/no-non-null-assertion': 'off',
       '@typescript-eslint/consistent-type-imports': 'error',
       'prettier/prettier': [
         'error',
