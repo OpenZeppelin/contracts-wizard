3bfac24c6e23e785c03f9d44d8602c7f393c7dad
Cove Marquis-Bortoli <44323490+CoveMB@users.noreply.github.com>
2025-02-21 16:47:17 -0500

    Run command utils (#454)
    Co-authored-by: Eric Lau <ericglau@outlook.com>

---
+++ scripts/language-input.mjs
@@ -0,0 +1,34 @@
+import { readdirSync, statSync } from "fs";
+import { join } from "path";
+
+const coreSubfolderPath = "./packages/core";
+
+function getSupportedLanguageInCoreSubfolder() {
+  return readdirSync(coreSubfolderPath).filter((subfolder) => {
+    const subfolderPath = join(coreSubfolderPath, subfolder);
+    return statSync(subfolderPath).isDirectory();
+  });
+}
+
+function getLanguageInput() {
+  const [, , languageFolderName] = process.argv;
+
+  if (languageFolderName) return languageFolderName;
+
+  console.error("Please provide a language name.");
+  process.exit(1);
+}
+
+export function getAndCheckLanguageInput() {
+  const languageFolderName = getLanguageInput();
+
+  const supportedLanguages = getSupportedLanguageInCoreSubfolder();
+
+  if (supportedLanguages.includes(languageFolderName))
+    return languageFolderName;
+
+  console.error(
+    `Invalid language name ${languageFolderName}. Supported languages: ${supportedLanguages.join(", ")}`
+  );
+  process.exit(1);
+}
