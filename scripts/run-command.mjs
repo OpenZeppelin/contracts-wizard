f7a0c8d73037d869c28c489c18d9ab2202ce5214
Cove Marquis-Bortoli <44323490+CoveMB@users.noreply.github.com>
2025-02-21 15:20:53 -0500

    Update scripts/run-command.mjs
    Co-authored-by: Eric Lau <ericglau@outlook.com>

--- scripts/run-command.mjs
+++ scripts/run-command.mjs
@@ -1,5 +1,5 @@
 import { commandAndFlagsInput } from "./command-input.mjs";
-import { inLanguageFolderRunTYarnCommand } from "./execute-command.mjs";
+import { runYarnCommandForLanguage } from "./execute-command.mjs";
 import { languageInput } from "./language-input.mjs";
 
 runYarnCommandForLanguage(languageInput, commandAndFlagsInput);
