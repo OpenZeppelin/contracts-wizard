e3ce7607cd7ae48bb6c9db26ea8bc816d00362dd
YanVictorSN <yanvictorsn@gmail.com>
2025-03-04 12:50:17 -0300

    fix: point to the module documentation

--- packages/ui/src/cairo/ERC20Controls.svelte
+++ packages/ui/src/cairo/ERC20Controls.svelte
@@ -86,7 +86,7 @@
       <span class="ml-1">
         <input type="checkbox" bind:checked={opts.votes}>
       </span>
-      <HelpTooltip align="right" link="https://docs.openzeppelin.com/contracts-cairo/api/governance#votes">
+      <HelpTooltip align="right" link="https://docs.openzeppelin.com/contracts-cairo/governance/votes">
         Keeps track of historical balances for voting in on-chain governance, with a way to delegate one's voting power to a trusted account.
       </HelpTooltip>
     </label>
