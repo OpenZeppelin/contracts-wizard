572d3ea4ebcbf2c6359263b3508176872f356a8c
immrsd <immrsd.eth@gmail.com>
2025-03-11 10:26:18 +0100

    Duplicate Cairo UI components for alpha support

---
+++ packages/ui/src/cairo_alpha/ERC20Controls.svelte
@@ -0,0 +1,114 @@
+<script lang="ts">
+  import HelpTooltip from '../common/HelpTooltip.svelte';
+
+  import type { KindedOptions, OptionsErrorMessages } from '@openzeppelin/wizard-cairo-alpha';
+  import { premintPattern, erc20, infoDefaults } from '@openzeppelin/wizard-cairo-alpha';
+
+  import AccessControlSection from './AccessControlSection.svelte';
+  import UpgradeabilityField from './UpgradeabilityField.svelte';
+  import InfoSection from './InfoSection.svelte';
+  import { error } from '../common/error-tooltip';
+
+  export const opts: Required<KindedOptions['ERC20']> = {
+    kind: 'ERC20',
+    ...erc20.defaults,
+    premint: '', // default to empty premint in UI instead of 0
+    info: { ...infoDefaults }, // create new object since Info is nested
+  };
+
+  export let errors: undefined | OptionsErrorMessages;
+
+  $: requireAccessControl = erc20.isAccessControlRequired(opts);
+</script>
+
+<section class="controls-section">
+  <h1>Settings</h1>
+
+  <div class="grid grid-cols-[2fr,1fr] gap-2">
+    <label class="labeled-input">
+      <span>Name</span>
+      <input bind:value={opts.name} use:error={errors?.name}>
+    </label>
+
+    <label class="labeled-input">
+      <span>Symbol</span>
+      <input bind:value={opts.symbol} use:error={errors?.symbol}>
+    </label>
+  </div>
+
+  <label class="labeled-input">
+    <span class="flex justify-between pr-2">
+      Premint
+      <HelpTooltip>Create an initial amount of tokens for the recipient.</HelpTooltip>
+    </span>
+    <input bind:value={opts.premint} use:error={errors?.premint} placeholder="0" pattern={premintPattern.source}>
+  </label>
+</section>
+
+<section class="controls-section">
+  <h1>Features</h1>
+
+  <div class="checkbox-group">
+    <label class:checked={opts.mintable}>
+      <input type="checkbox" bind:checked={opts.mintable}>
+      Mintable
+      <HelpTooltip link="https://docs.openzeppelin.com/contracts-cairo/guides/erc20-supply">
+        Privileged accounts will be able to create more supply.
+      </HelpTooltip>
+    </label>
+
+    <label class:checked={opts.burnable}>
+      <input type="checkbox" bind:checked={opts.burnable}>
+      Burnable
+      <HelpTooltip>
+        Token holders will be able to destroy their tokens.
+      </HelpTooltip>
+    </label>
+
+    <label class:checked={opts.pausable}>
+      <input type="checkbox" bind:checked={opts.pausable}>
+      Pausable
+      <HelpTooltip link="https://docs.openzeppelin.com/contracts-cairo/security#pausable">
+        Privileged accounts will be able to pause the functionality marked with <code>self.pausable.assert_not_paused()</code>.
+        Useful for emergency response.
+      </HelpTooltip>
+    </label>
+
+    <UpgradeabilityField bind:upgradeable={opts.upgradeable} />
+  </div>
+</section>
+
+<section class="controls-section">
+  <h1>
+    <!-- svelte-ignore a11y-label-has-associated-control -->
+    <label class="flex items-center tooltip-container pr-2">
+      <span>Votes</span>
+      <span class="ml-1">
+        <input type="checkbox" bind:checked={opts.votes}>
+      </span>
+      <HelpTooltip align="right" link="https://docs.openzeppelin.com/contracts-cairo/governance/votes">
+        Keeps track of historical balances for voting in on-chain governance, with a way to delegate one's voting power to a trusted account.
+      </HelpTooltip>
+    </label>
+  </h1>
+
+  <label class="labeled-input">
+    <span class="flex justify-between pr-2">
+      Application Name
+      <HelpTooltip link="https://docs.openzeppelin.com/contracts-cairo/guides/snip12">Name for domain separator. Prevents two applications from producing the same hash.</HelpTooltip>
+    </span>
+    <input bind:value={opts.appName} use:error={errors?.appName} disabled={!opts.votes}>
+  </label>
+
+  <label class="labeled-input">
+    <span class="flex justify-between pr-2">
+      Application Version
+      <HelpTooltip link="https://docs.openzeppelin.com/contracts-cairo/guides/snip12">Version for domain separator. Prevents two versions of the same application from producing the same hash.</HelpTooltip>
+    </span>
+    <input bind:value={opts.appVersion} use:error={errors?.appVersion} disabled={!opts.votes}>
+  </label>
+</section>
+
+<AccessControlSection bind:access={opts.access} required={requireAccessControl} />
+
+<InfoSection bind:info={opts.info} />
---
+++ packages/ui/src/cairo_alpha/inject-hyperlinks.ts
@@ -0,0 +1,44 @@
+import { contractsVersionTag } from '@openzeppelin/wizard-cairo-alpha/src';
+
+export function injectHyperlinks(code: string) {
+  const importRegex = /use<\/span> (openzeppelin)::([^A-Z]*)(::[a-zA-Z0-9]+|::{)/g;
+  let result = code;
+  let match = importRegex.exec(code);
+  while (match != null) {
+    const [line, libraryPrefix, libraryPath, suffix] = match;
+    if (line !== undefined && libraryPrefix !== undefined && libraryPath !== undefined && suffix !== undefined) {
+      const githubPrefix = `https://github.com/OpenZeppelin/cairo-contracts/blob/${contractsVersionTag}/packages/`;
+
+      const libraryPathSegments = libraryPath.split('::');
+      libraryPathSegments.splice(1, 0, 'src');
+
+      if (libraryPathSegments !== undefined && libraryPathSegments.length > 0) {
+        let replacement;
+        if (suffix === '::{') {
+          // Multiple components are imported, so remove components and link to the parent .cairo file
+          replacement = `use</span> <a class="import-link" href='${githubPrefix}${libraryPathSegments.join(
+            '/',
+          )}.cairo' target='_blank' rel='noopener noreferrer'>${libraryPrefix}::${libraryPath}</a>${suffix}`; // Exclude suffix from link
+        } else {
+          // Single component is imported
+          // If a mapping exists, link to the mapped file, otherwise remove the component and link to the parent .cairo file
+          const componentName = suffix.substring(2, suffix.length);
+          const mapping = componentMappings[componentName];
+          const urlSuffix = mapping ? `/${mapping}.cairo` : '.cairo';
+          replacement = `use</span> <a class="import-link" href='${githubPrefix}${libraryPathSegments.join(
+            '/',
+          )}${urlSuffix}' target='_blank' rel='noopener noreferrer'>${libraryPrefix}::${libraryPath}${suffix}</a>`; // Include suffix (component) in link
+        }
+
+        result = result.replace(line, replacement);
+      }
+    }
+    match = importRegex.exec(code);
+  }
+  return result;
+}
+
+const componentMappings: { [key: string]: string } = {
+  AccountComponent: 'account',
+  UpgradeableComponent: 'upgradeable',
+} as const;
