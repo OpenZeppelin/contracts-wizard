23e341dc37bd860d04d4efe15f631b07282a519c
immrsd <immrsd.eth@gmail.com>
2025-03-11 10:26:43 +0100

    Introduce VersionSwitch control

---
+++ packages/ui/src/common/VersionSwitch.svelte
@@ -0,0 +1,77 @@
+<script>
+  export let left;
+  export let right;
+
+  let tab = left;
+
+  function switchTo(page) {
+    tab = page;
+  }
+</script>
+
+<div class="container overflow-hidden">
+  <div class="header flex flex-row justify-between overflow-hidden">
+    <div class="button-container">
+      <button class:selected={tab === left} on:click={() => switchTo(left)}>
+        {left.name}
+      </button>
+      <button class:selected={tab === right} on:click={() => switchTo(right)}>
+        {right.name}
+      </button>
+    </div>
+  </div>
+  <div class="page-container flex flex-col justify-between overflow-hidden">
+    <svelte:component this={tab.page} />
+  </div>
+</div>
+
+<style lang="postcss">
+  .container {
+    background-color: var(--gray-1);
+    min-width: 32rem;
+  }
+
+  .button-container {
+    height: 36px;
+    margin-left: 0;
+    margin-right: 0;
+    margin-top: 0;
+    margin-bottom: 4px;
+    font-size: 0;
+    color: var(--gray-5);
+    background-color: black;
+    display: inline-block;
+    border: 1px solid var(--gray-4);
+    border-radius: 6px;
+    padding: 0px;
+    overflow: hidden;
+  }
+
+  .button-container button, :global(.overflow-btn) {
+    font-size: 16px;
+    height: 100%;
+    padding: 0 var(--size-3);
+    border-radius: 0px;
+    cursor: pointer;
+    transition: none;
+    border: none;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  .button-container button, :global(.overflow-btn) {
+    border: 0;
+    background-color: var(--gray-1);
+  }
+
+  .button-container button:hover, :global(.overflow-btn):hover {
+    background-color: var(--gray-2);
+  }
+
+  .button-container button.selected {
+    background-color: var(--blue-2);
+    color: white;
+    order: -1;
+  }
+
+</style>
