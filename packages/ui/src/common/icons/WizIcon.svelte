79879ac21ed8b0a9ae4bc861d8129f1b32d619bb
Eric Lau <ericglau@outlook.com>
2025-03-18 11:08:53 -0400

    Make label sizes more consistent

--- packages/ui/src/common/icons/WizIcon.svelte
+++ packages/ui/src/common/icons/WizIcon.svelte
@@ -1,4 +1,4 @@
-<svg xmlns="http://www.w3.org/2000/svg" class="vertical-align" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+<svg xmlns="http://www.w3.org/2000/svg" class="vertical-align" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
   <path d="m21.64 3.64-1.28-1.28a1.21 1.21 0 0 0-1.72 0L2.36 18.64a1.21 1.21 0 0 0 0 1.72l1.28 1.28a1.2 1.2 0 0 0 1.72 0L21.64 5.36a1.2 1.2 0 0 0 0-1.72Z"/>
   <path d="m14 7 3 3"/>
   <path d="M5 6v4"/>
