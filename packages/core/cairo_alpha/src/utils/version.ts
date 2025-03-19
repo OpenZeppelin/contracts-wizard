e3be4492d26f8cf47d44882780eaae43603e6404
immrsd <immrsd.eth@gmail.com>
2025-03-19 11:24:23 +0100

    Update Cairo & Scarb versions

--- packages/core/cairo_alpha/src/utils/version.ts
+++ packages/core/cairo_alpha/src/utils/version.ts
@@ -8,8 +8,8 @@ export const contractsVersionTag = `v${contractsVersion}`;
  * Cairo compiler versions.
  */
 export const edition = '2024_07';
-export const cairoVersion = '2.9.4';
-export const scarbVersion = '2.9.4';
+export const cairoVersion = '2.11.2';
+export const scarbVersion = '2.11.3';
 
 /**
  * Semantic version string representing of the minimum compatible version of Contracts to display in output.
