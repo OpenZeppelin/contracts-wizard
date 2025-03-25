48c04d45332aeb31634df84031e2695e859ffef8
immrsd <immrsd.eth@gmail.com>
2025-03-25 09:15:57 +0100

    Update ERC721 generator logic

--- packages/core/cairo/src/generate/erc721.ts
+++ packages/core/cairo/src/generate/erc721.ts
@@ -2,28 +2,36 @@ import type { ERC721Options } from '../erc721';
 import { accessOptions } from '../set-access-control';
 import { infoOptions } from '../set-info';
 import { upgradeableOptions } from '../set-upgradeable';
-import { royaltyInfoOptions } from '../set-royalty-info';
+import { RoyaltyInfoSubset, resolveRoyaltyOptionsSubset } from '../set-royalty-info';
 import { generateAlternatives } from './alternatives';
 
 const booleans = [true, false];
 
-const blueprint = {
-  name: ['MyToken'],
-  symbol: ['MTK'],
-  baseUri: ['https://example.com/'],
-  burnable: booleans,
-  enumerable: booleans,
-  votes: booleans,
-  appName: ['MyApp'],
-  appVersion: ['v1'],
-  pausable: booleans,
-  mintable: booleans,
-  royaltyInfo: [royaltyInfoOptions.disabled, royaltyInfoOptions.enabledDefault, royaltyInfoOptions.enabledCustom],
-  access: accessOptions,
-  upgradeable: upgradeableOptions,
-  info: infoOptions,
+type GeneratorOptions = {
+  royaltyInfo: RoyaltyInfoSubset,
 };
 
-export function* generateERC721Options(): Generator<Required<ERC721Options>> {
+function prepareBlueprint(opts: GeneratorOptions) {
+  const royaltyInfo = resolveRoyaltyOptionsSubset(opts.royaltyInfo);
+  return {
+    name: ['MyToken'],
+    symbol: ['MTK'],
+    baseUri: ['https://example.com/'],
+    burnable: booleans,
+    enumerable: booleans,
+    votes: booleans,
+    appName: ['MyApp'],
+    appVersion: ['v1'],
+    pausable: booleans,
+    mintable: booleans,
+    royaltyInfo,
+    access: accessOptions,
+    upgradeable: upgradeableOptions,
+    info: infoOptions,
+  };
+}
+
+export function* generateERC721Options(opts: GeneratorOptions): Generator<Required<ERC721Options>> {
+  const blueprint = prepareBlueprint(opts);
   yield* generateAlternatives(blueprint);
 }
