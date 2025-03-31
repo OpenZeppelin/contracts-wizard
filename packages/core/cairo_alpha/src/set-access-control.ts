92c018874756a8769964b6b401f6355c22f71d28
immrsd <immrsd.eth@gmail.com>
2025-03-31 09:10:02 +0200

    Support AccessControlWithDelay for Cairo-alpha

--- packages/core/cairo_alpha/src/set-access-control.ts
+++ packages/core/cairo_alpha/src/set-access-control.ts
@@ -31,6 +31,10 @@ export function setAccessControl(c: ContractBuilder, access: Access): void {
             name: 'AccessControlCamelImpl',
             value: 'AccessControlComponent::AccessControlCamelImpl<ContractState>',
           });
+          c.addImplToComponent(components.AccessControlComponent, {
+            name: 'AccessControlWithDelay',
+            value: 'AccessControlComponent::AccessControlWithDelay',
+          });
         } else {
           c.addImplToComponent(components.AccessControlComponent, {
             name: 'AccessControlMixinImpl',
