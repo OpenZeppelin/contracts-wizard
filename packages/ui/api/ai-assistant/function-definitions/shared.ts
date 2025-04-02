3d935b103e5002207a8d257ff99e1dbd8b9b8177
CoveMB <bmarquiscom@gmail.com>
2025-04-01 21:50:39 -0400

    remove cruf

--- packages/ui/api/ai-assistant/function-definitions/shared.ts
+++ packages/ui/api/ai-assistant/function-definitions/shared.ts
@@ -1,5 +1,5 @@
 import type { AiFunctionPropertyDefinition } from '../types/function-definition.ts';
-import type { SolidityCommonOptions, AllLanguageContractOptions } from '../types/languages.ts';
+import type { SolidityCommonOptions } from '../types/languages.ts';
 
 export const commonFunctionDescription = {
   access: {
