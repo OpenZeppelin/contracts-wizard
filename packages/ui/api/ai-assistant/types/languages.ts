c7a690574ec0e8930ad5c9ea1a068867045a64aa
ernestognw <ernestognw@gmail.com>
2025-04-09 17:26:41 -0600

    update language.ts

--- packages/ui/api/ai-assistant/types/languages.ts
+++ packages/ui/api/ai-assistant/types/languages.ts
@@ -3,9 +3,10 @@ export type { CommonOptions as SolidityCommonOptions } from '../../../../core/so
 
 // Add supported language here
 export type LanguagesContractsOptions = {
-  solidity: Omit<SolidityKindedOptions, 'Stablecoin' | 'RealWorldAsset'> & {
+  solidity: Omit<SolidityKindedOptions, 'Stablecoin' | 'RealWorldAsset' | 'Account'> & {
     Stablecoin: Omit<SolidityKindedOptions['Stablecoin'], 'upgradeable'> & { upgradeable?: false };
     RealWorldAsset: Omit<SolidityKindedOptions['RealWorldAsset'], 'upgradeable'> & { upgradeable?: false };
+    Account: Omit<SolidityKindedOptions['Account'], 'upgradeable'> & { upgradeable?: false };
   };
 };
 
