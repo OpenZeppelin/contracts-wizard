e9d1709b344ad5302a35436318c070082d2a9d3c
Eric Lau <ericglau@outlook.com>
2025-05-09 16:58:52 -0400

    add erc20 with basic parameters

--- packages/mcp/src/index.ts
+++ packages/mcp/src/index.ts
@@ -1,17 +1,83 @@
 import { McpServer, ResourceTemplate } from '@modelcontextprotocol/sdk/server/mcp.js';
 import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js';
 import { z } from 'zod';
+import type { KindedOptions } from '@openzeppelin/wizard';
+import { erc20 } from '@openzeppelin/wizard';
 
 // Create an MCP server
 const server = new McpServer({
-  name: 'Demo',
-  version: '1.0.0',
+  name: 'OpenZeppelin Contracts Wizard',
+  version: '0.0.1',
 });
 
 // Add an addition tool
-server.tool('add', { a: z.number(), b: z.number() }, async ({ a, b }) => ({
-  content: [{ type: 'text', text: String(a + b) }],
-}));
+server.tool(
+  'generateERC20',
+  {
+    name: z.string(),
+    symbol: z.string(),
+    burnable: z.boolean(),
+    pausable: z.boolean(),
+    premint: z.string(),
+    premintChainId: z.string(),
+    mintable: z.boolean(),
+    callback: z.boolean(),
+    permit: z.boolean(),
+    // votes: z.boolean().or(z.enum(['blocknumber', 'timestamp'])),
+    // flashmint: z.boolean(),
+    // crossChainBridging: z.literal(false).or(z.enum(['custom', 'superchain'])),
+    // access: z.literal(false).or(z.enum(['ownable', 'roles', 'managed'])),
+    // upgradeable: z.literal(false).or(z.enum(['transparent', 'uups'])),
+    // info: z.object({
+    //   securityContact: z.string().optional(),
+    //   license: z.string().optional(),
+    // }),
+  },
+  async ({
+    name,
+    symbol,
+    burnable,
+    pausable,
+    premint,
+    premintChainId,
+    mintable,
+    callback,
+    permit,
+    // votes,
+    // flashmint,
+    // crossChainBridging,
+    // access,
+    // upgradeable,
+    // info,
+  }) => {
+    const opts: KindedOptions['ERC20'] = {
+      kind: 'ERC20',
+      name,
+      symbol,
+      burnable,
+      pausable,
+      premint,
+      premintChainId,
+      mintable,
+      callback,
+      permit,
+      // votes,
+      // flashmint,
+      // crossChainBridging,
+      // access,
+      // upgradeable,
+      // info,
+    };
+    return {
+      content: [
+        {
+          type: 'text',
+          text: erc20.print(opts),
+        },
+      ],
+    };
+  },
+);
 
 // Add a dynamic greeting resource
 server.resource('greeting', new ResourceTemplate('greeting://{name}', { list: undefined }), async (uri, { name }) => ({
