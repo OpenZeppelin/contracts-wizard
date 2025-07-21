408b1f31327bfd695006c1e90c8c0c95348c3e65
brozorec <9572072+brozorec@users.noreply.github.com>
2025-07-21 15:33:33 +0200

    version and changeset

--- packages/core/stellar/src/utils/version.ts
+++ packages/core/stellar/src/utils/version.ts
@@ -1,13 +1,13 @@
 /**
  * The actual latest version to use in links.
  */
-export const contractsVersion = '0.3.0';
+export const contractsVersion = '0.4.0';
 export const contractsVersionTag = `v${contractsVersion}`;
 
 /**
  * Semantic version string representing of the minimum compatible version of Contracts to display in output.
  */
-export const compatibleContractsSemver = '^0.3.0';
+export const compatibleContractsSemver = '^0.4.0';
 
 /**
  * The Soroban version for which compilation and testing have passing tests
