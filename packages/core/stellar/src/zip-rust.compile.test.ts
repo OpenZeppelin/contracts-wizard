b1eb7f8b220b37a1e6df04939d788bfee6b08156
CoveMB <bmarquiscom@gmail.com>
2025-08-04 14:30:23 +0200

    test no change

--- packages/core/stellar/src/zip-rust.compile.test.ts
+++ packages/core/stellar/src/zip-rust.compile.test.ts
@@ -3,7 +3,7 @@ import test from 'ava';
 import { runRustCompilationTest } from './utils/compile-test';
 
 test.serial(
-  'rust zip environment',
+  'rust zip',
   runRustCompilationTest(
     buildFungible,
     {
