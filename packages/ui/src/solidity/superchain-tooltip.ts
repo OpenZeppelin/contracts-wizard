c4b49d7006062b018f685233c1ba163ce5fe55d5
Eric Lau <ericglau@outlook.com>
2025-08-06 13:17:11 -0400

    Change string concat from code review

--- packages/ui/src/solidity/superchain-tooltip.ts
+++ packages/ui/src/solidity/superchain-tooltip.ts
@@ -11,12 +11,12 @@ export const superchainGenericTooltipProps: { [prop: string]: string | boolean }
 };
 
 const ERC20_REQUIREMENTS =
-  ' Requires deploying your contract to the same address on every chain in the Superchain. <a class="light-link" href="https://docs.optimism.io/stack/interop/superchain-erc20#requirements" target="_blank" rel="noopener noreferrer">Read more.</a>';
+  'Requires deploying your contract to the same address on every chain in the Superchain. <a class="light-link" href="https://docs.optimism.io/stack/interop/superchain-erc20#requirements" target="_blank" rel="noopener noreferrer">Read more.</a>';
 
 /**
  * Tippy.js properties for the SuperchainERC20 tooltip.
  */
 export const superchainERC20TooltipProps: { [prop: string]: string | boolean } = {
   ...superchainGenericTooltipProps,
-  content: superchainGenericTooltipProps.content + ERC20_REQUIREMENTS,
+  content: superchainGenericTooltipProps.content + ' ' + ERC20_REQUIREMENTS,
 };
