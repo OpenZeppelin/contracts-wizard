7872cfdd2e09f3a2cf96b7d1158edd50c5dfaf6f
Eric Lau <ericglau@outlook.com>
2025-08-12 15:07:50 -0400

    Improve inject hyperlinks logic

--- packages/ui/src/solidity/inject-hyperlinks.ts
+++ packages/ui/src/solidity/inject-hyperlinks.ts
@@ -1,5 +1,4 @@
 import { version as contractsVersion } from '@openzeppelin/contracts/package.json';
-import { getCommunityContractsGitCommit } from '@openzeppelin/wizard/src/utils/community-contracts-git-commit';
 
 export function injectHyperlinks(code: string) {
   // We are modifying HTML, so use HTML escaped chars. The pattern excludes paths that include /../ in the URL.
@@ -8,19 +7,27 @@ export function injectHyperlinks(code: string) {
   const importCommunityContractsRegex =
     /&quot;(@openzeppelin\/)(community-contracts\/)((?:(?!\.\.)[^/]+\/)*?[^/]*?)&quot;/g;
 
-  const compatibleCommunityContractsRegex = /OpenZeppelin Community Contracts commit ([a-f0-9]{40})/g;
+  const compatibleCommunityContractsRegexSingle = /OpenZeppelin Community Contracts commit ([a-f0-9]{40})/;
+  const compatibleCommunityContractsRegexGlobal = new RegExp(compatibleCommunityContractsRegexSingle, 'g');
 
-  return code
-    .replace(
-      importContractsRegex,
-      `&quot;<a class="import-link" href="https://github.com/OpenZeppelin/openzeppelin-$2blob/v${contractsVersion}/contracts/$3" target="_blank" rel="noopener noreferrer">$1$2$3</a>&quot;`,
-    )
-    .replace(
-      importCommunityContractsRegex,
-      `&quot;<a class="import-link" href="https://github.com/OpenZeppelin/openzeppelin-community-contracts/tree/${getCommunityContractsGitCommit()}/contracts/$3" target="_blank" rel="noopener noreferrer">$1$2$3</a>&quot;`,
-    )
-    .replace(
-      compatibleCommunityContractsRegex,
-      `OpenZeppelin Community Contracts commit <a class="import-link" href="https://github.com/OpenZeppelin/openzeppelin-community-contracts/tree/$1" target="_blank" rel="noopener noreferrer" title="View repository at commit $1">$1</a>`,
-    );
+  const compatibleCommunityContractsGitCommit = code.match(compatibleCommunityContractsRegexSingle)?.[1];
+
+  let result = code.replace(
+    importContractsRegex,
+    `&quot;<a class="import-link" href="https://github.com/OpenZeppelin/openzeppelin-$2blob/v${contractsVersion}/contracts/$3" target="_blank" rel="noopener noreferrer">$1$2$3</a>&quot;`,
+  );
+
+  if (compatibleCommunityContractsGitCommit !== undefined) {
+    result = result
+      .replace(
+        importCommunityContractsRegex,
+        `&quot;<a class="import-link" href="https://github.com/OpenZeppelin/openzeppelin-community-contracts/tree/${compatibleCommunityContractsGitCommit}/contracts/$3" target="_blank" rel="noopener noreferrer">$1$2$3</a>&quot;`,
+      )
+      .replace(
+        compatibleCommunityContractsRegexGlobal,
+        `OpenZeppelin Community Contracts commit <a class="import-link" href="https://github.com/OpenZeppelin/openzeppelin-community-contracts/tree/$1" target="_blank" rel="noopener noreferrer" title="View repository at commit $1">$1</a>`,
+      );
+  }
+
+  return result;
 }
