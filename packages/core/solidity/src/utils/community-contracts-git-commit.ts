22e663984cfe7022df687840d91fcf9cc14a584d
Eric Lau <ericglau@outlook.com>
2025-08-12 17:01:53 -0400

    Add stricter commit hash format check

--- packages/core/solidity/src/utils/community-contracts-git-commit.ts
+++ packages/core/solidity/src/utils/community-contracts-git-commit.ts
@@ -9,11 +9,17 @@ export function getCommunityContractsGitCommit(): string {
 }
 
 function extractGitCommitHash(dependencyName: string, dependencyVersion: string): string {
-  const splitHash = dependencyVersion.split('#');
-  if (!dependencyVersion.startsWith('git+') || splitHash.length !== 2) {
+  const split = dependencyVersion.split('#');
+  if (!dependencyVersion.startsWith('git+') || split.length !== 2) {
     throw new Error(
       `Expected package dependency for ${dependencyName} in format git+<url>#<commit-hash>, but got ${dependencyVersion}`,
     );
   }
-  return splitHash[1]!;
+  const hash = split[1]!;
+  if (!/^[0-9a-f]{40}$/.test(hash)) {
+    throw new Error(
+      `Expected git commit hash for package dependency ${dependencyName} to have 40 hex chars, but got ${hash}`,
+    );
+  }
+  return hash;
 }
