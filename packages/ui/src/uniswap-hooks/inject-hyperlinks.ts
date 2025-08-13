45d7d64b1720d33ea0ea489c763aba0c796acafc
Gonzalo Othacehe <gonza.otc@gmail.com>
2025-08-13 16:55:17 -0300

    add uniswap hooks hyperlinks

--- packages/ui/src/uniswap-hooks/inject-hyperlinks.ts
+++ packages/ui/src/uniswap-hooks/inject-hyperlinks.ts
@@ -5,6 +5,8 @@ export function injectHyperlinks(code: string) {
   const contractsRegex =
     /&quot;(@openzeppelin\/)(contracts-upgradeable\/|contracts\/)((?:(?!\.\.)[^/]+\/)*?[^/]*?)&quot;/g;
   const communityContractsRegex = /&quot;(@openzeppelin\/)(community-contracts\/)((?:(?!\.\.)[^/]+\/)*?[^/]*?)&quot;/g;
+  const uniswapV4CoreRegex = /&quot;(@uniswap\/)(v4-core\/contracts\/)((?:(?!\.\.)[^/]+\/)*?[^/]*?)&quot;/g;
+  const ozUniswapHooksRegex = /&quot;(@openzeppelin\/)(uniswap-hooks\/src\/)((?:(?!\.\.)[^/]+\/)*?[^/]*?)&quot;/g;
 
   return code
     .replace(
@@ -14,5 +16,13 @@ export function injectHyperlinks(code: string) {
     .replace(
       communityContractsRegex,
       `&quot;<a class="import-link" href="https://github.com/OpenZeppelin/openzeppelin-community-contracts/blob/master/contracts/$3" target="_blank" rel="noopener noreferrer">$1$2$3</a>&quot;`,
+    )
+    .replace(
+      uniswapV4CoreRegex,
+      `&quot;<a class="import-link" href="https://github.com/Uniswap/v4-core/blob/main/src/$3" target="_blank" rel="noopener noreferrer">$1$2$3</a>&quot;`,
+    )
+    .replace(
+      ozUniswapHooksRegex,
+      `&quot;<a class="import-link" href="https://github.com/OpenZeppelin/uniswap-hooks/blob/master/src/$3" target="_blank" rel="noopener noreferrer">$1$2$3</a>&quot;`,
     );
 }
