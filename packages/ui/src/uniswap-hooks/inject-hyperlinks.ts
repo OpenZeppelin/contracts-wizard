4adb61617358db35d952575ee121cd0fc422643b
Gonzalo Othacehe <gonza.otc@gmail.com>
2025-08-25 15:37:35 -0300

    removed openzeppelin community from inject hyperlinks

--- packages/ui/src/uniswap-hooks/inject-hyperlinks.ts
+++ packages/ui/src/uniswap-hooks/inject-hyperlinks.ts
@@ -4,7 +4,6 @@ export function injectHyperlinks(code: string) {
   // We are modifying HTML, so use HTML escaped chars. The pattern excludes paths that include /../ in the URL.
   const contractsRegex =
     /&quot;(@openzeppelin\/)(contracts-upgradeable\/|contracts\/)((?:(?!\.\.)[^/]+\/)*?[^/]*?)&quot;/g;
-  const communityContractsRegex = /&quot;(@openzeppelin\/)(community-contracts\/)((?:(?!\.\.)[^/]+\/)*?[^/]*?)&quot;/g;
   const ozUniswapHooksRegex = /&quot;(@openzeppelin\/)(uniswap-hooks\/)((?:(?!\.\.)[^/]+\/)*?[^/]*?)&quot;/g;
   const uniswapV4CoreRegex = /&quot;(@uniswap\/)(v4-core\/src\/)((?:(?!\.\.)[^/]+\/)*?[^/]*?)&quot;/g;
 
@@ -12,10 +11,6 @@ export function injectHyperlinks(code: string) {
     .replace(
       contractsRegex,
       `&quot;<a class="import-link" href="https://github.com/OpenZeppelin/openzeppelin-$2blob/v${contractsVersion}/contracts/$3" target="_blank" rel="noopener noreferrer">$1$2$3</a>&quot;`,
-    )
-    .replace(
-      communityContractsRegex,
-      `&quot;<a class="import-link" href="https://github.com/OpenZeppelin/openzeppelin-community-contracts/blob/master/contracts/$3" target="_blank" rel="noopener noreferrer">$1$2$3</a>&quot;`,
     )
     .replace(
       uniswapV4CoreRegex,
