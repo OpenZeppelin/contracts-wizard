9fb1e1f0bc7e7667440b2acfadccbfff53b0a558
Eric Lau <ericglau@outlook.com>
2025-09-29 09:42:17 -0400

    Use hardhat-polkadot 0.2.0-pre1

--- packages/core/solidity/src/zip-hardhat-polkadot.ts
+++ packages/core/solidity/src/zip-hardhat-polkadot.ts
@@ -18,7 +18,7 @@ class HardhatPolkadotZipGenerator extends HardhatZipGenerator {
   },
   networks: {
     hardhat: {
-      polkavm: true,
+      polkadot: true,
       nodeConfig: {
         nodeBinaryPath: 'INSERT_PATH_TO_SUBSTRATE_NODE',
         rpcPort: 8000,
