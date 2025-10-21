95bc5af16dd50ba7909a8f95a2c896e6da12e6ab
immrsd <immrsd.eth@gmail.com>
2025-10-21 12:01:41 +0300

    Update set-acess-control to support AccessControlDefaultAdminRules

--- packages/core/cairo_alpha/src/set-access-control.ts
+++ packages/core/cairo_alpha/src/set-access-control.ts
@@ -1,23 +1,60 @@
 import type { BaseFunction, BaseImplementedTrait, ContractBuilder } from './contract';
 import { defineComponents } from './utils/define-components';
 import { addSRC5Component } from './common-components';
+import { durationToSeconds } from './utils/duration';
+import { OptionsError } from './error';
+import { toUint } from './utils/convert-strings';
 
-export const accessOptions = [false, 'ownable', 'roles', 'roles-default-admin-rules'] as const;
+export type AccessType =
+  | false
+  | 'ownable'
+  | 'roles' // AccessControl
+  | 'roles-dar'; // AccessControlDefaultAdminRules
+
+export type RolesDefaultAdminRulesOptions = {
+  darInitialDelay: string;
+  darDefaultDelayIncrease: string;
+};
+
+export type Access = { type: AccessType } & RolesDefaultAdminRulesOptions;
+
+export const darDefaultOpts: RolesDefaultAdminRulesOptions = {
+  darInitialDelay: '1 day',
+  darDefaultDelayIncrease: '5 days',
+};
+
+export const darCustomOps: RolesDefaultAdminRulesOptions = {
+  darInitialDelay: '2 days',
+  darDefaultDelayIncrease: '1 week',
+};
+
+export const AccessControl = {
+  None: { type: false, ...darDefaultOpts } as const,
+  Ownable: { type: 'ownable', ...darDefaultOpts } as const,
+  Roles: { type: 'roles', ...darDefaultOpts } as const,
+  RolesDefaultAdminRules: (opts: RolesDefaultAdminRulesOptions) => {
+    return { type: 'roles-dar', ...opts } as const;
+  },
+} as const;
+
 export const DEFAULT_ACCESS_CONTROL = 'ownable';
+export const accessOptions = [
+  AccessControl.None,
+  AccessControl.Ownable,
+  AccessControl.Roles,
+  AccessControl.RolesDefaultAdminRules(darDefaultOpts),
+  AccessControl.RolesDefaultAdminRules(darCustomOps),
+] as const;
 
-export type Access = (typeof accessOptions)[number];
+const DEFAULT_ADMIN_DELAY_INCREASE_WAIT = BigInt(5 * 24 * 60 * 60); // 5 days
 
-/**
- * Sets access control for the contract by adding inheritance.
- */
+/// Sets access control for the contract by adding inheritance.
 export function setAccessControl(c: ContractBuilder, access: Access): void {
-  switch (access) {
+  switch (access.type) {
     case 'ownable': {
       c.addComponent(components.OwnableComponent, [{ lit: 'owner' }], true);
-
       c.addUseClause('starknet', 'ContractAddress');
       c.addConstructorArgument({ name: 'owner', type: 'ContractAddress' });
-
       break;
     }
     case 'roles': {
@@ -55,7 +92,7 @@ export function setAccessControl(c: ContractBuilder, access: Access): void {
       }
       break;
     }
-    case 'roles-default-admin-rules': {
+    case 'roles-dar': {
       const initParams = [{ lit: 'INITIAL_DELAY' }, { lit: 'initial_default_admin' }];
       if (c.addComponent(components.AccessControlDefaultAdminRulesComponent, initParams, true)) {
         if (c.interfaceFlags.has('ISRC5')) {
@@ -83,9 +120,40 @@ export function setAccessControl(c: ContractBuilder, access: Access): void {
           c.addInterfaceFlag('ISRC5');
         }
         addSRC5Component(c);
+        const initialDelay = toUint(getInitialDelay(access), 'darInitialDelay', 'u64');
+        c.addConstant({
+          name: 'INITIAL_DELAY',
+          type: 'u64',
+          value: initialDelay.toString(),
+          comment: access.darInitialDelay,
+          inlineComment: true,
+        });
+        const defaultAdminDelayIncreaseWait = toUint(
+          getDefaultAdminDelayIncreaseWait(access),
+          'darDefaultDelayIncrease',
+          'u64',
+        );
+        if (defaultAdminDelayIncreaseWait == DEFAULT_ADMIN_DELAY_INCREASE_WAIT) {
+          c.addUseClause('openzeppelin::access::accesscontrol::extensions', 'DefaultConfig', {
+            alias: 'AccessControlDefaultAdminRulesDefaultConfig',
+          });
+        } else {
+          const trait: BaseImplementedTrait = {
+            name: 'AccessControlDefaultAdminRulesImmutableConfig',
+            of: 'AccessControlDefaultAdminRulesComponent::ImmutableConfig',
+            tags: [],
+          };
+          c.addImplementedTrait(trait);
+          c.addSuperVariableToTrait(trait, {
+            name: 'DEFAULT_ADMIN_DELAY_INCREASE_WAIT',
+            type: 'u64',
+            value: defaultAdminDelayIncreaseWait.toString(),
+            comment: access.darDefaultDelayIncrease,
+            inlineComment: true,
+          });
+        }
 
         c.addUseClause('starknet', 'ContractAddress');
-        c.addConstructorArgument({ name: 'initial_default_admin', type: 'ContractAddress' });
         c.addConstructorArgument({ name: 'initial_default_admin', type: 'ContractAddress' });
       }
       break;
@@ -100,31 +168,33 @@ export function requireAccessControl(
   c: ContractBuilder,
   trait: BaseImplementedTrait,
   fn: BaseFunction,
-  access: Access,
+  accessObj: Access,
   roleIdPrefix: string,
   roleOwner: string | undefined,
 ): void {
-  if (access === false) {
-    access = DEFAULT_ACCESS_CONTROL;
+  const access = { ...accessObj }; // make a copy to avoid triggering UI updates
+  if (access.type === false) {
+    access.type = DEFAULT_ACCESS_CONTROL;
   }
   setAccessControl(c, access);
 
-  switch (access) {
+  switch (access.type) {
     case 'ownable': {
       c.addFunctionCodeBefore(trait, fn, 'self.ownable.assert_only_owner()');
       break;
     }
     case 'roles':
-    case 'roles-default-admin-rules': {
+    case 'roles-dar': {
       const roleId = roleIdPrefix + '_ROLE';
       const addedSuper = c.addSuperVariable({
         name: roleId,
         type: 'felt252',
         value: `selector!("${roleId}")`,
       });
-      const substorageName = access === 'roles-default-admin-rules' 
-        ? components.AccessControlDefaultAdminRulesComponent.substorage.name 
-        : components.AccessControlComponent.substorage.name;
+      const substorageName =
+        access.type === 'roles-dar'
+          ? components.AccessControlDefaultAdminRulesComponent.substorage.name
+          : components.AccessControlComponent.substorage.name;
       if (roleOwner !== undefined) {
         c.addUseClause('starknet', 'ContractAddress');
         c.addConstructorArgument({ name: roleOwner, type: 'ContractAddress' });
@@ -136,6 +206,34 @@ export function requireAccessControl(
       c.addFunctionCodeBefore(trait, fn, `self.${substorageName}.assert_only_role(${roleId})`);
 
       break;
+    }
+  }
+}
+
+function getInitialDelay(opts: RolesDefaultAdminRulesOptions): number {
+  try {
+    return durationToSeconds(opts.darInitialDelay);
+  } catch (e) {
+    if (e instanceof Error) {
+      throw new OptionsError({
+        darInitialDelay: e.message,
+      });
+    } else {
+      throw e;
+    }
+  }
+}
+
+function getDefaultAdminDelayIncreaseWait(opts: RolesDefaultAdminRulesOptions): number {
+  try {
+    return durationToSeconds(opts.darDefaultDelayIncrease);
+  } catch (e) {
+    if (e instanceof Error) {
+      throw new OptionsError({
+        darDefaultDelayIncrease: e.message,
+      });
+    } else {
+      throw e;
     }
   }
 }
@@ -198,5 +296,5 @@ const components = defineComponents({
         value: 'AccessControlDefaultAdminRulesComponent::InternalImpl<ContractState>',
       },
     ],
-  }
+  },
 });
