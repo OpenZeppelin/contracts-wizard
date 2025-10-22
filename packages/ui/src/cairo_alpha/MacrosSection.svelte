ccbc8fdef45caac9e1c26865b959ead7fce62881
immrsd <immrsd.eth@gmail.com>
2025-10-22 14:55:29 +0300

    Add UI control for macros section

---
+++ packages/ui/src/cairo_alpha/MacrosSection.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import type { MacrosOptions } from '@openzeppelin/wizard-cairo-alpha';
+  import HelpTooltip from '../common/HelpTooltip.svelte';
+
+  export let macros: MacrosOptions;
+</script>
+
+<section class="controls-section">
+  <h1>Macros</h1>
+
+  <div class="checkbox-group">
+    <label class:checked={macros.withComponents}>
+      <input type="checkbox" bind:checked={macros.withComponents} />
+      with_components
+      <HelpTooltip link="https://docs.openzeppelin.com/contracts-cairo/alpha/macros/with_components">
+        Use the "with_components" macro for simplified contract structure.
+      </HelpTooltip>
+    </label>
+  </div>
+</section>
+
