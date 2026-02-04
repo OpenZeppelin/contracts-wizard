06a52eafa66ed1d7756500e4c2abac9bc2d4f2c4
immrsd <immrsd.eth@gmail.com>
2026-02-04 16:50:58 +0300

    Turn ERC1155 supply feature on in Cairo-alpha tests with components ON

--- packages/core/cairo_alpha/src/tests/with_components_on/erc1155/erc1155.test.ts
+++ packages/core/cairo_alpha/src/tests/with_components_on/erc1155/erc1155.test.ts
@@ -17,6 +17,7 @@ const allFeaturesON: Partial<ERC1155Options> = {
   mintable: true,
   burnable: true,
   pausable: true,
+  supply: true,
   royaltyInfo: royaltyInfoOptions.enabledDefault,
   upgradeable: true,
 } as const;
@@ -196,6 +197,7 @@ test('API isAccessControlRequired', async t => {
     }),
     true,
   );
+  t.is(erc1155.isAccessControlRequired({ updatableUri: false, supply: false }), false);
   t.is(erc1155.isAccessControlRequired({ updatableUri: false }), false);
   t.is(erc1155.isAccessControlRequired({}), true); // updatableUri is true by default
 });
